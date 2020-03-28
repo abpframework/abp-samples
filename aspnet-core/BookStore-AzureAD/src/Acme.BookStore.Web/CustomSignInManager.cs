064577653a745996c03a66b211fa35ff3879ef48
Galip Tolga Erdem <galiperdem@gmail.com>
2020-03-28 15:40:44 +0300

    Initial Commit

---
+++ aspnet-core/BookStore-AzureAD/src/Acme.BookStore.Web/CustomSignInManager.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Acme.BookStore.Web
+{
+    public class CustomSignInManager : SignInManager<Volo.Abp.Identity.IdentityUser>
+    {
+        private const string LoginProviderKey = "LoginProvider";
+        private const string XsrfKey = "XsrfId";
+        public CustomSignInManager(
+            UserManager<Volo.Abp.Identity.IdentityUser> userManager,
+            Microsoft.AspNetCore.Http.IHttpContextAccessor contextAccessor,
+            IUserClaimsPrincipalFactory<Volo.Abp.Identity.IdentityUser> claimsFactory,
+            Microsoft.Extensions.Options.IOptions<IdentityOptions> optionsAccessor,
+            Microsoft.Extensions.Logging.ILogger<SignInManager<Volo.Abp.Identity.IdentityUser>> logger,
+            Microsoft.AspNetCore.Authentication.IAuthenticationSchemeProvider schemes,
+            IUserConfirmation<Volo.Abp.Identity.IdentityUser> confirmation) : base(userManager, contextAccessor, claimsFactory, optionsAccessor, logger, schemes, confirmation)
+        {
+        }
+        // https://github.com/aspnet/Identity/blob/feedcb5c53444f716ef5121d3add56e11c7b71e5/src/Identity/SignInManager.cs#L589-L624
+        public override async Task<ExternalLoginInfo> GetExternalLoginInfoAsync(string expectedXsrf = null)
+        {
+            var auth = await Context.AuthenticateAsync(IdentityConstants.ExternalScheme);
+            var items = auth?.Properties?.Items;
+            if (auth?.Principal == null || items == null || !items.ContainsKey(LoginProviderKey))
+            {
+                return null;
+            }
+
+            if (expectedXsrf != null)
+            {
+                if (!items.ContainsKey(XsrfKey))
+                {
+                    return null;
+                }
+                var userId = items[XsrfKey] as string;
+                if (userId != expectedXsrf)
+                {
+                    return null;
+                }
+            }
+
+            var providerKey = auth.Principal.FindFirstValue(ClaimTypes.NameIdentifier);
+            var provider = items[LoginProviderKey] as string;
+            if (providerKey == null || provider == null)
+            {
+                return null;
+            }
+
+            var providerDisplayName = (await GetExternalAuthenticationSchemesAsync()).FirstOrDefault(p => p.Name == provider)?.DisplayName
+                                      ?? provider;
+            return new ExternalLoginInfo(auth.Principal, provider, providerKey, providerDisplayName)
+            {
+                AuthenticationTokens = auth.Properties.GetTokens()
+            };
+        }
+
+
+    }
+}
---
+++ aspnet-core/BookStore-AzureAD/src/Acme.BookStore.Web/Pages/CustomLoginModel.cs
@@ -0,0 +1,41 @@
+﻿using Volo.Abp.Account.Web.Pages.Account;
+using Volo.Abp.DependencyInjection;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+using Volo.Abp.Security.Claims;
+using IdentityUser = Volo.Abp.Identity.IdentityUser;
+using Microsoft.AspNetCore.Mvc;
+using IdentityModel;
+
+namespace Acme.BookStore.Web
+{
+    public class CustomLoginModel : LoginModel
+    {
+        public CustomLoginModel(
+            Microsoft.AspNetCore.Authentication.IAuthenticationSchemeProvider schemeProvider,
+            Microsoft.Extensions.Options.IOptions<Volo.Abp.Account.Web.AbpAccountOptions> accountOptions)
+            : base(schemeProvider, accountOptions)
+        {
+        }
+
+        protected override async Task<Volo.Abp.Identity.IdentityUser> CreateExternalUserAsync(ExternalLoginInfo info)
+        {
+            var emailAddress = info.Principal.FindFirstValue(AbpClaimTypes.Email);
+            var userId = GuidGenerator.Create();
+            var user = new IdentityUser(userId, emailAddress, emailAddress, CurrentTenant.Id);
+            user.Name = info.Principal.FindFirstValue(JwtClaimTypes.GivenName);
+            user.Surname = info.Principal.FindFirstValue(JwtClaimTypes.FamilyName);
+
+            //Optional: Add claims to user claims
+            user.Claims.Add(new Volo.Abp.Identity.IdentityUserClaim(GuidGenerator.Create(), userId, JwtClaimTypes.GivenName, info.Principal.FindFirstValue(JwtClaimTypes.GivenName), CurrentTenant.Id));
+            user.Claims.Add(new Volo.Abp.Identity.IdentityUserClaim(GuidGenerator.Create(), userId, JwtClaimTypes.FamilyName, info.Principal.FindFirstValue(JwtClaimTypes.FamilyName), CurrentTenant.Id));
+
+            CheckIdentityErrors(await UserManager.CreateAsync(user));
+            CheckIdentityErrors(await UserManager.SetEmailAsync(user, emailAddress));
+            CheckIdentityErrors(await UserManager.AddLoginAsync(user, info));
+
+            return user;
+        }
+    }
+}

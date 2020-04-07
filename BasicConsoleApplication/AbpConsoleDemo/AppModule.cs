c15f9b170eeec43aed22ef126af97c3710eba2c3
Akın Sabri Çam <sabricam2016@gmail.com>
2020-04-07 11:12:38 +0300

    moved coreapp and console samples in this repository

---
+++ BasicConsoleApplication/AbpConsoleDemo/AppModule.cs
@@ -0,0 +1,13 @@
+﻿using Volo.Abp.Autofac;
+using Volo.Abp.Modularity;
+
+namespace AbpConsoleDemo
+{
+    [DependsOn(
+        typeof(AbpAutofacModule)
+        )]
+    public class AppModule : AbpModule
+    {
+
+    }
+}

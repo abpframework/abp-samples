2774556a65cfc88218639d443200d57329f094aa
Akın Sabri Çam <sabricam2016@gmail.com>
2020-04-07 22:22:15 +0300

    Moved bookstore-angular-mongodb sample

---
+++ BookStore-Angular-MongoDb/angular/src/app/books/books-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+
+import { BooksComponent } from "./books.component";
+import { BookListComponent } from "./book-list/book-list.component";
+
+const routes: Routes = [
+  {
+    path: "",
+    component: BooksComponent,
+    children: [{ path: "", component: BookListComponent }]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class BooksRoutingModule {}

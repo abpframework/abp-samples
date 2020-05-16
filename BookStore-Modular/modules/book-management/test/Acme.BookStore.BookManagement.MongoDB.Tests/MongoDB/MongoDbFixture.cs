5e1a0131dab9dcafd7e13ded77877634478af5c1
maliming <6908465+maliming@users.noreply.github.com>
2020-05-16 21:18:13 +0800

    Add MongoTestCollection.

---
+++ BookStore-Modular/modules/book-management/test/Acme.BookStore.BookManagement.MongoDB.Tests/MongoDB/MongoDbFixture.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Mongo2Go;
+
+namespace Acme.BookStore.BookManagement.MongoDB
+{
+    public class MongoDbFixture : IDisposable
+    {
+        private static readonly MongoDbRunner MongoDbRunner = MongoDbRunner.Start();
+        public static readonly string ConnectionString = MongoDbRunner.ConnectionString;
+
+        public void Dispose()
+        {
+            MongoDbRunner?.Dispose();
+        }
+    }
+}
---
+++ BookStore-Modular/modules/book-management/test/Acme.BookStore.BookManagement.MongoDB.Tests/MongoDB/MongoTestCollection.cs
@@ -0,0 +1,10 @@
+﻿using Xunit;
+
+namespace Acme.BookStore.BookManagement.MongoDB
+{
+    [CollectionDefinition(Name)]
+    public class MongoTestCollection : ICollectionFixture<MongoDbFixture>
+    {
+        public const string Name = "MongoDB Collection";
+    }
+}

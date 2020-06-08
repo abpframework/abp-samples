0c7c42d062f59c4a79dde1f28420827186bb7d50
Yunus Emre Kalkan <emre.kalkan@volosoft.com>
2020-06-08 13:17:26 +0300

    SignalRTieredDemo removed rabbitmq from identityserver

--- SignalRTieredDemo/src/SignalRTieredDemo.Web/ChatHub.cs
+++ SignalRTieredDemo/src/SignalRTieredDemo.Web/ChatHub.cs
@@ -1,7 +1,5 @@
-﻿using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using Volo.Abp.AspNetCore.SignalR;
-using Volo.Abp.EventBus.Distributed;
 
 namespace SignalRTieredDemo.Web
 {

dd04c65aa6b544de0bd8ca42a03e4075052721fb
mehmet-erim <bilgi@mehmeterim.com>
2020-06-30 12:44:23 +0300

    feat: add route.provider

--- BookStore-Angular-MongoDb/angular/src/app/app-routing.module.ts
+++ BookStore-Angular-MongoDb/angular/src/app/app-routing.module.ts
@@ -15,19 +15,22 @@ const routes: Routes = [
   },
   {
     path: 'account',
-    loadChildren: () => import('@abp/ng.account').then((m) => m.AccountModule),
+    loadChildren: () =>
+      import('@abp/ng.account').then((m) => m.AccountModule.forLazy({ redirectUrl: '/' })),
   },
   {
     path: 'identity',
-    loadChildren: () => import('@abp/ng.identity').then((m) => m.IdentityModule),
+    loadChildren: () => import('@abp/ng.identity').then((m) => m.IdentityModule.forLazy()),
   },
   {
     path: 'tenant-management',
-    loadChildren: () => import('@abp/ng.tenant-management').then((m) => m.TenantManagementModule),
+    loadChildren: () =>
+      import('@abp/ng.tenant-management').then((m) => m.TenantManagementModule.forLazy()),
   },
   {
     path: 'setting-management',
-    loadChildren: () => import('@abp/ng.setting-management').then((m) => m.SettingManagementModule),
+    loadChildren: () =>
+      import('@abp/ng.setting-management').then((m) => m.SettingManagementModule.forLazy()),
   },
   {
     path: 'books',
--- BookStore-Angular-MongoDb/angular/src/app/app.module.ts
+++ BookStore-Angular-MongoDb/angular/src/app/app.module.ts
@@ -1,9 +1,10 @@
-import { AccountConfigModule } from '@abp/ng.account.config';
+import { AccountConfigModule } from '@abp/ng.account/config';
 import { CoreModule } from '@abp/ng.core';
-import { IdentityConfigModule } from '@abp/ng.identity.config';
-import { SettingManagementConfigModule } from '@abp/ng.setting-management.config';
-import { TenantManagementConfigModule } from '@abp/ng.tenant-management.config';
+import { IdentityConfigModule } from '@abp/ng.identity/config';
+import { SettingManagementConfigModule } from '@abp/ng.setting-management/config';
+import { TenantManagementConfigModule } from '@abp/ng.tenant-management/config';
 import { ThemeSharedModule } from '@abp/ng.theme.shared';
+import { ThemeBasicModule } from '@abp/ng.theme.basic';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -12,10 +13,7 @@ import { NgxsModule } from '@ngxs/store';
 import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared.module';
-import { BookState } from './book/state/book.state';
-
-const LOGGERS = [NgxsLoggerPluginModule.forRoot({ disabled: false })];
+import { APP_ROUTE_PROVIDER } from './route.provider';
 
 @NgModule({
   imports: [
@@ -26,14 +24,14 @@ const LOGGERS = [NgxsLoggerPluginModule.forRoot({ disabled: false })];
       environment,
     }),
     ThemeSharedModule.forRoot(),
-    AccountConfigModule.forRoot({ redirectUrl: '/' }),
-    IdentityConfigModule,
-    TenantManagementConfigModule,
-    SettingManagementConfigModule,
-    NgxsModule.forRoot([BookState]),
-    SharedModule,
-    ...(environment.production ? [] : LOGGERS),
+    AccountConfigModule.forRoot(),
+    IdentityConfigModule.forRoot(),
+    TenantManagementConfigModule.forRoot(),
+    SettingManagementConfigModule.forRoot(),
+    ThemeBasicModule.forRoot(),
+    NgxsModule.forRoot(),
   ],
+  providers: [APP_ROUTE_PROVIDER],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
 })

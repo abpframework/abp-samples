fd6979cbde27b74557bc35095b5d75ecf50b138c
Akın Sabri Çam <sabricam2016@gmail.com>
2020-07-02 01:04:27 +0300

    changed again this method access type.

--- Authentication-Customization/test/Acme.BookStore.TestBase/Security/FakeCurrentPrincipalAccessor.cs
+++ Authentication-Customization/test/Acme.BookStore.TestBase/Security/FakeCurrentPrincipalAccessor.cs
@@ -8,7 +8,7 @@ namespace Acme.BookStore.Security
     [Dependency(ReplaceServices = true)]
     public class FakeCurrentPrincipalAccessor : ThreadCurrentPrincipalAccessor
     {
-        protected override ClaimsPrincipal GetClaimsPrincipal()
+        public override ClaimsPrincipal GetClaimsPrincipal()
         {
             return GetPrincipal();
         }

792c873f05a30953aa711cc18a171bf62212966b
Galip Tolga Erdem <galiperdem@gmail.com>
2020-07-02 18:19:06 +0300

    Update FakeCurrentPrincipalAccessor.cs

--- OrganizationUnitSample/test/OrganizationUnitSample.TestBase/Security/FakeCurrentPrincipalAccessor.cs
+++ OrganizationUnitSample/test/OrganizationUnitSample.TestBase/Security/FakeCurrentPrincipalAccessor.cs
@@ -8,11 +8,6 @@ namespace OrganizationUnitSample.Security
     [Dependency(ReplaceServices = true)]
     public class FakeCurrentPrincipalAccessor : ThreadCurrentPrincipalAccessor
     {
-        public override ClaimsPrincipal GetClaimsPrincipal()
-        {
-            return GetPrincipal();
-        }
-
         private ClaimsPrincipal _principal;
 
         private ClaimsPrincipal GetPrincipal()
@@ -27,9 +22,9 @@ namespace OrganizationUnitSample.Security
                             new ClaimsIdentity(
                                 new List<Claim>
                                 {
-                                    new Claim(AbpClaimTypes.UserId,"2e701e62-0953-4dd3-910b-dc6cc93ccb0d"),
-                                    new Claim(AbpClaimTypes.UserName,"admin"),
-                                    new Claim(AbpClaimTypes.Email,"admin@abp.io")
+                                    new Claim(AbpClaimTypes.UserId, "2e701e62-0953-4dd3-910b-dc6cc93ccb0d"),
+                                    new Claim(AbpClaimTypes.UserName, "admin"),
+                                    new Claim(AbpClaimTypes.Email, "admin@abp.io")
                                 }
                             )
                         );

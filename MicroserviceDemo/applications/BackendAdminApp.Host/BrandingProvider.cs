3f80e8f32d201746cecd9296a0ccb688e10239d0
Galip Tolga Erdem <galiperdem@gmail.com>
2020-07-02 19:33:49 +0300

    Added Microservice Demo and updated local references to nuget packages

---
+++ MicroserviceDemo/applications/BackendAdminApp.Host/BrandingProvider.cs
@@ -0,0 +1,10 @@
+﻿using Volo.Abp.AspNetCore.Mvc.UI.Theme.Shared.Components;
+using Volo.Abp.DependencyInjection;
+
+namespace BackendAdminApp.Host
+{
+    public class BrandingProvider : DefaultBrandingProvider, ISingletonDependency
+    {
+        public override string AppName => "Backend Admin App";
+    }
+}

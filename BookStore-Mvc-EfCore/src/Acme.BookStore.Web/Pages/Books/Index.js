5cf1052a38b58a66afc56925aa65744e5382fd26
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2020-07-05 00:32:43 +0300

    Update Index.js

--- BookStore-Mvc-EfCore/src/Acme.BookStore.Web/Pages/Books/Index.js
+++ BookStore-Mvc-EfCore/src/Acme.BookStore.Web/Pages/Books/Index.js
@@ -5,80 +5,84 @@
 
     var dataTable = $('#BooksTable').DataTable(
         abp.libs.datatables.normalizeConfiguration({
-                ajax: abp.libs.datatables.createAjax(acme.bookStore.books.book.getList),
-                columnDefs: [
-                    {
-                        title: l('Actions'),
-                        rowAction: {
-                            items:
-                                [
-                                    {
-                                        text: l('Edit'),
-                                        action: function (data) {
-                                            editModal.open({ id: data.record.id });
-                                        }
-                                    },
-                                    {
-                                        text: l('Delete'),
-                                        confirmMessage: function (data) {
-                                            return l(
-                                                'BookDeletionConfirmationMessage',
-                                                data.record.name
-                                            );
-                                        },
-                                        action: function (data) {
-                                            acme.bookStore.books.book
-                                                .delete(data.record.id)
-                                                .then(function() {
-                                                    abp.notify.info(
-                                                        l('SuccessfullyDeleted')
-                                                    );
-                                                    dataTable.ajax.reload();
-                                                });
-                                        }
+            serverSide: true,
+            paging: true,
+            order: [[1, "asc"]],
+            searching: false,
+            scrollX: true,
+            ajax: abp.libs.datatables.createAjax(acme.bookStore.books.book.getList),
+            columnDefs: [
+                {
+                    title: l('Actions'),
+                    rowAction: {
+                        items:
+                            [
+                                {
+                                    text: l('Edit'),
+                                    action: function (data) {
+                                        editModal.open({ id: data.record.id });
                                     }
-                                ]
-                        }
-                    },
-                    {
-                        title: l('Name'),
-                        data: "name"
-                    },
-                    {
-                        title: l('Type'),
-                        data: "type",
-                        render: function (data) {
-                            return l('Enum:BookType:' + data);
-                        }
-                    },
-                    {
-                        title: l('PublishDate'),
-                        data: "publishDate",
-                        render: function (data) {
-                            return luxon
-                                .DateTime
-                                .fromISO(data, {
-                                    locale: abp.localization.currentCulture.name
-                                }).toLocaleString();
-                        }
-                    },
-                    {
-                        title: l('Price'),
-                        data: "price"
-                    },
-                    {
-                        title: l('CreationTime'), data: "creationTime",
-                        render: function (data) {
-                            return luxon
-                                .DateTime
-                                .fromISO(data, {
-                                    locale: abp.localization.currentCulture.name
-                                }).toLocaleString(luxon.DateTime.DATETIME_SHORT);
-                        }
+                                },
+                                {
+                                    text: l('Delete'),
+                                    confirmMessage: function (data) {
+                                        return l(
+                                            'BookDeletionConfirmationMessage',
+                                            data.record.name
+                                        );
+                                    },
+                                    action: function (data) {
+                                        acme.bookStore.books.book
+                                            .delete(data.record.id)
+                                            .then(function() {
+                                                abp.notify.info(
+                                                    l('SuccessfullyDeleted')
+                                                );
+                                                dataTable.ajax.reload();
+                                            });
+                                    }
+                                }
+                            ]
                     }
-                ]
-            }
-        )
+                },
+                {
+                    title: l('Name'),
+                    data: "name"
+                },
+                {
+                    title: l('Type'),
+                    data: "type",
+                    render: function (data) {
+                        return l('Enum:BookType:' + data);
+                    }
+                },
+                {
+                    title: l('PublishDate'),
+                    data: "publishDate",
+                    render: function (data) {
+                        return luxon
+                            .DateTime
+                            .fromISO(data, {
+                                locale: abp.localization.currentCulture.name
+                            }).toLocaleString();
+                    }
+                },
+                {
+                    title: l('Price'),
+                    data: "price"
+                },
+                {
+                    title: l('CreationTime'), data: "creationTime",
+                    render: function (data) {
+                        return luxon
+                            .DateTime
+                            .fromISO(data, {
+                                locale: abp.localization.currentCulture.name
+                            }).toLocaleString(luxon.DateTime.DATETIME_SHORT);
+                    }
+                }
+            ]
+        })
     );
 
     createModal.onResult(function () {
@@ -93,5 +97,4 @@
         e.preventDefault();
         createModal.open();
     });
-
 });

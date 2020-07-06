6a65c153ad45ca00738315f6b98f96aa5f09af0d
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2020-07-06 18:08:52 +0300

    Completed book creation.

--- BookStore-Angular-MongoDb2/angular/src/app/book/book.component.ts
+++ BookStore-Angular-MongoDb2/angular/src/app/book/book.component.ts
@@ -2,21 +2,36 @@ import { ListService, PagedResultDto } from '@abp/ng.core';
 import { Component, OnInit } from '@angular/core';
 import { BookDto, BookType } from './models';
 import { BookService } from './services';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { NgbDateNativeAdapter, NgbDateAdapter } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-book',
   templateUrl: './book.component.html',
   styleUrls: ['./book.component.scss'],
-  providers: [ListService],
+  providers: [
+    ListService,
+    { provide: NgbDateAdapter, useClass: NgbDateNativeAdapter }
+  ],
 })
 export class BookComponent implements OnInit {
   book = { items: [], totalCount: 0 } as PagedResultDto<BookDto>;
 
   booksType = BookType;
 
-  isModalOpen = false; // <== added this line ==>
+  form: FormGroup;
 
-  constructor(public readonly list: ListService, private bookService: BookService) {}
+  bookTypes = Object.keys(BookType).filter(
+    (bookType) => typeof this.booksType[bookType] === 'number'
+  );
+
+  isModalOpen = false;
+
+  constructor(
+    public readonly list: ListService,
+    private bookService: BookService,
+    private fb: FormBuilder
+  ) {}
 
   ngOnInit() {
     const bookStreamCreator = (query) => this.bookService.getListByInput(query);
@@ -26,8 +41,29 @@ export class BookComponent implements OnInit {
     });
   }
 
-  // added createBook method
   createBook() {
+    this.buildForm();
     this.isModalOpen = true;
+  }
+
+  buildForm() {
+    this.form = this.fb.group({
+      name: ['', Validators.required],
+      type: [null, Validators.required],
+      publishDate: [null, Validators.required],
+      price: [null, Validators.required],
+    });
+  }
+
+  save() {
+    if (this.form.invalid) {
+      return;
+    }
+
+    this.bookService.createByInput(this.form.value).subscribe(() => {
+      this.isModalOpen = false;
+      this.form.reset();
+      this.list.get();
+    });
   }
 }
--- BookStore-Angular-MongoDb2/angular/src/app/book/book.module.ts
+++ BookStore-Angular-MongoDb2/angular/src/app/book/book.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { BookRoutingModule } from './book-routing.module';
 import { BookComponent } from './book.component';
+import { NgbDatepickerModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [BookComponent],
   imports: [
     BookRoutingModule,
-    SharedModule
+    SharedModule,
+    NgbDatepickerModule,
   ]
 })
 export class BookModule { }

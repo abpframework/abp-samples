231c09e7eae7ebc153512e11888dc551da99e74c
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2020-07-20 23:33:30 +0300

    Completed the application layer.

--- BookStore-Mvc-EfCore/src/Acme.BookStore.Application.Contracts/Permissions/BookStorePermissions.cs
+++ BookStore-Mvc-EfCore/src/Acme.BookStore.Application.Contracts/Permissions/BookStorePermissions.cs
@@ -11,5 +11,13 @@
             public const string Edit = Default + ".Edit";
             public const string Delete = Default + ".Delete";
         }
+
+        public static class Authors
+        {
+            public const string Default = GroupName + ".Authors";
+            public const string Create = Default + ".Create";
+            public const string Edit = Default + ".Edit";
+            public const string Delete = Default + ".Delete";
+        }
     }
 }
--- BookStore-Mvc-EfCore/src/Acme.BookStore.Domain/BookStoreDataSeederContributor.cs
+++ BookStore-Mvc-EfCore/src/Acme.BookStore.Domain/BookStoreDataSeederContributor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Acme.BookStore.Authors;
 using Acme.BookStore.Books;
 using Volo.Abp.Data;
 using Volo.Abp.DependencyInjection;
@@ -11,13 +12,50 @@ namespace Acme.BookStore
         : IDataSeedContributor, ITransientDependency
     {
         private readonly IRepository<Book, Guid> _bookRepository;
+        private readonly IAuthorRepository _authorRepository;
+        private readonly AuthorManager _authorManager;
 
-        public BookStoreDataSeederContributor(IRepository<Book, Guid> bookRepository)
+        public BookStoreDataSeederContributor(
+            IRepository<Book, Guid> bookRepository,
+            IAuthorRepository authorRepository,
+            AuthorManager authorManager)
         {
             _bookRepository = bookRepository;
+            _authorRepository = authorRepository;
+            _authorManager = authorManager;
         }
 
         public async Task SeedAsync(DataSeedContext context)
+        {
+            await CreateAuthorsAsync();
+            await CreateBooksAsync();
+        }
+
+        private async Task CreateAuthorsAsync()
+        {
+            if (await _authorRepository.GetCountAsync() > 0)
+            {
+                return;
+            }
+
+            await _authorRepository.InsertAsync(
+                await _authorManager.CreateAsync(
+                    "George Orwell",
+                    new DateTime(1903, 06, 25),
+                    "Orwell produced literary criticism and poetry, fiction and polemical journalism; and is best known for the allegorical novella Animal Farm (1945) and the dystopian novel Nineteen Eighty-Four (1949)."
+                )
+            );
+
+            await _authorRepository.InsertAsync(
+                await _authorManager.CreateAsync(
+                    "Douglas Adams",
+                    new DateTime(1952, 03, 11),
+                    "Douglas Adams was an English author, screenwriter, essayist, humorist, satirist and dramatist. Adams was an advocate for environmentalism and conservation, a lover of fast cars, technological innovation and the Apple Macintosh, and a self-proclaimed 'radical atheist'."
+                )
+            );
+        }
+
+        private async Task CreateBooksAsync()
         {
             if (await _bookRepository.GetCountAsync() > 0)
             {

76852108d623ce6b468f874397a829bd4b10b9dd
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2020-07-20 09:24:04 +0300

    Create Author.cs

---
+++ BookStore-Mvc-EfCore/src/Acme.BookStore.Domain/Authors/Author.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace Acme.BookStore.Authors
+{
+    public class Author : FullAuditedAggregateRoot<Guid>
+    {
+        public string Name { get; internal set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public string ShortBio { get; set; }
+    }
+}

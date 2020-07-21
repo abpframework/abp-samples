6274edc19a6cd828c82b93aff53cae94bd1aac99
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2020-07-21 19:38:54 +0300

    Implemented the authors page.

---
+++ BookStore-Mvc-EfCore/src/Acme.BookStore.Web/Pages/Authors/Index.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Acme.BookStore.Web.Pages.Authors
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}

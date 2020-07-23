61225e40889a7072e6418f8d30a76e24102e0919
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2020-07-23 11:48:56 +0300

    Complete the author functionality.

--- BookStore-Angular-MongoDb/angular/src/app/author/author.component.ts
+++ BookStore-Angular-MongoDb/angular/src/app/author/author.component.ts
@@ -2,23 +2,91 @@ import { Component, OnInit } from '@angular/core';
 import { ListService, PagedResultDto } from '@abp/ng.core';
 import { AuthorDto } from './models';
 import { AuthorService } from './services';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { NgbDateNativeAdapter, NgbDateAdapter } from '@ng-bootstrap/ng-bootstrap';
+import { ConfirmationService, Confirmation } from '@abp/ng.theme.shared';
 
 @Component({
   selector: 'app-author',
   templateUrl: './author.component.html',
   styleUrls: ['./author.component.scss'],
-  providers: [ListService],
+  providers: [ListService, { provide: NgbDateAdapter, useClass: NgbDateNativeAdapter }],
 })
 export class AuthorComponent implements OnInit {
   author = { items: [], totalCount: 0 } as PagedResultDto<AuthorDto>;
 
-  constructor(public readonly list: ListService, private authorService: AuthorService) {}
+  isModalOpen = false;
+
+  form: FormGroup;
+
+  selectedAuthor = new AuthorDto();
+
+  constructor(
+    public readonly list: ListService,
+    private authorService: AuthorService,
+    private fb: FormBuilder,
+    private confirmation: ConfirmationService
+  ) {}
 
   ngOnInit(): void {
     const authorStreamCreator = (query) => this.authorService.getListByInput(query);
 
     this.list.hookToQuery(authorStreamCreator).subscribe((response) => {
       this.author = response;
+    });
+  }
+
+  createAuthor() {
+    this.selectedAuthor = new AuthorDto();
+    this.buildForm();
+    this.isModalOpen = true;
+  }
+
+  editAuthor(id: string) {
+    this.authorService.getById(id).subscribe((author) => {
+      this.selectedAuthor = author;
+      this.buildForm();
+      this.isModalOpen = true;
+    });
+  }
+
+  buildForm() {
+    this.form = this.fb.group({
+      name: [this.selectedAuthor.name || '', Validators.required],
+      birthDate: [
+        this.selectedAuthor.birthDate ? new Date(this.selectedAuthor.birthDate) : null,
+        Validators.required,
+      ],
+    });
+  }
+
+  save() {
+    if (this.form.invalid) {
+      return;
+    }
+
+    if (this.selectedAuthor.id) {
+      this.authorService
+        .updateByIdAndInput(this.form.value, this.selectedAuthor.id)
+        .subscribe(() => {
+          this.isModalOpen = false;
+          this.form.reset();
+          this.list.get();
+        });
+    } else {
+      this.authorService.createByInput(this.form.value).subscribe(() => {
+        this.isModalOpen = false;
+        this.form.reset();
+        this.list.get();
+      });
+    }
+  }
+
+  delete(id: string) {
+    this.confirmation.warn('::AreYouSureToDelete', '::AreYouSure').subscribe((status) => {
+      if (status === Confirmation.Status.confirm) {
+        this.authorService.deleteById(id).subscribe(() => this.list.get());
+      }
     });
   }
 }
--- BookStore-Angular-MongoDb/angular/src/app/author/author.module.ts
+++ BookStore-Angular-MongoDb/angular/src/app/author/author.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { AuthorRoutingModule } from './author-routing.module';
 import { AuthorComponent } from './author.component';
+import { NgbDatepickerModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [AuthorComponent],
-  imports: [SharedModule, AuthorRoutingModule],
+  imports: [SharedModule, AuthorRoutingModule, NgbDatepickerModule],
 })
 export class AuthorModule {}

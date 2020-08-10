ccf0ab23280767b33b1a72c11c9283fd19c8e80a
liangshiwei <liangshiw@outlook.com>
2020-08-10 10:00:59 +0800

    Update common.ps1

--- build/common.ps1
+++ build/common.ps1
@@ -7,7 +7,6 @@ $rootFolder = (Get-Item -Path "./" -Verbose).FullName
 $solutionPaths = (
     "../Authentication-Customization",
     "../BasicAspNetCoreApplication",
-    "../BasicConsoleApplication",
     "../BookStore",
     "../BookStore-Angular-MongoDb/aspnet-core",
     "../BookStore-Modular/modules/book-management",

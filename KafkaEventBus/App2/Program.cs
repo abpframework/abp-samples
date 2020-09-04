51938cb91fa0d214cd938fb9d30e290794183531
maliming <6908465+maliming@users.noreply.github.com>
2020-09-04 16:49:24 +0800

    Initial Kafka sample.

---
+++ KafkaEventBus/App2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp;
+
+namespace App2
+{
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            using (var application = AbpApplicationFactory.Create<App2Module>(options =>
+            {
+                options.UseAutofac();
+            }))
+            {
+                application.Initialize();
+
+                var messagingService = application
+                    .ServiceProvider
+                    .GetRequiredService<App2MessagingService>();
+
+                await messagingService.RunAsync();
+
+                application.Shutdown();
+            }
+        }
+    }
+}

2261023b26c1c1395bfcc1da53930caa05f61a5b
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2020-09-18 16:37:05 +0300

    Added new sample project: BookStore with Blazor & EF Core.

---
+++ BookStore-Blazor-EfCore/src/Acme.BookStore.Blazor/BookStoreMenuContributor.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using Acme.BookStore.Localization;
+using Volo.Abp.UI.Navigation;
+
+namespace Acme.BookStore.Blazor
+{
+    public class BookStoreMenuContributor : IMenuContributor
+    {
+        public Task ConfigureMenuAsync(MenuConfigurationContext context)
+        {
+            if(context.Menu.DisplayName != StandardMenus.Main)
+            {
+                return Task.CompletedTask;
+            }
+
+            var l = context.GetLocalizer<BookStoreResource>();
+
+            context.Menu.Items.Insert(
+                0,
+                new ApplicationMenuItem(
+                    "BookStore.Home",
+                    l["Menu:Home"],
+                    "/",
+                    icon: "fas fa-home"
+                )
+            );
+
+            return Task.CompletedTask;
+        }
+    }
+}

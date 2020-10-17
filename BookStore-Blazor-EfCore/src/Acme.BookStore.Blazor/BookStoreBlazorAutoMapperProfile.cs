b2c1ed300ba12842eb48b1bbe134edae9f10a27b
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2020-10-17 17:34:11 +0300

    Added initial Blazor BookStore sample with abp framework v3.3.0-rc.1

---
+++ BookStore-Blazor-EfCore/src/Acme.BookStore.Blazor/BookStoreBlazorAutoMapperProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace Acme.BookStore.Blazor
+{
+    public class BookStoreBlazorAutoMapperProfile : Profile
+    {
+        public BookStoreBlazorAutoMapperProfile()
+        {
+            //Define your AutoMapper configuration here for the Blazor project.
+        }
+    }
+}

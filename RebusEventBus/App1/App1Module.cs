f550912e3c52786fc6103fa7cc4de071fa37abcf
maliming <malimings@gmail.com>
2020-10-28 15:16:26 +0800

    Init Rebus solution.

---
+++ RebusEventBus/App1/App1Module.cs
@@ -0,0 +1,21 @@
+﻿using Volo.Abp.Autofac;
+using Volo.Abp.EventBus.Rebus;
+using Volo.Abp.Modularity;
+
+namespace App1
+{
+    [DependsOn(
+        typeof(AbpEventBusRebusModule),
+        typeof(AbpAutofacModule)
+        )]
+    public class App1Module : AbpModule
+    {
+        public override void PreConfigureServices(ServiceConfigurationContext context)
+        {
+            PreConfigure<AbpRebusEventBusOptions>(options =>
+            {
+                options.InputQueueName = "eventbus";
+            });
+        }
+    }
+}
---
+++ RebusEventBus/App2/App2Module.cs
@@ -0,0 +1,21 @@
+﻿using Volo.Abp.Autofac;
+using Volo.Abp.EventBus.Rebus;
+using Volo.Abp.Modularity;
+
+namespace App2
+{
+    [DependsOn(
+        typeof(AbpEventBusRebusModule),
+        typeof(AbpAutofacModule)
+        )]
+    public class App2Module : AbpModule
+    {
+        public override void PreConfigureServices(ServiceConfigurationContext context)
+        {
+            PreConfigure<AbpRebusEventBusOptions>(options =>
+            {
+                options.InputQueueName = "eventbus";
+            });
+        }
+    }
+}

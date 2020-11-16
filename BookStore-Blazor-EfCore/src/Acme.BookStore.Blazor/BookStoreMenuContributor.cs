aa1943f44c29194d715e1b3150e4ec452a842c9e
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2020-11-16 17:36:30 +0300

    Implemented bookstore blazor ui part 2.

--- BookStore-Blazor-EfCore/src/Acme.BookStore.Blazor/BookStoreMenuContributor.cs
+++ BookStore-Blazor-EfCore/src/Acme.BookStore.Blazor/BookStoreMenuContributor.cs
@@ -44,6 +44,20 @@ namespace Acme.BookStore.Blazor
                 )
             );
 
+            context.Menu.AddItem(
+                new ApplicationMenuItem(
+                    "BooksStore",
+                    l["Menu:BookStore"],
+                    icon: "fa fa-book"
+                ).AddItem(
+                    new ApplicationMenuItem(
+                        "BooksStore.Books",
+                        l["Menu:Books"],
+                        url: "/books"
+                    )
+                )
+            );
+
             return Task.CompletedTask;
         }
 

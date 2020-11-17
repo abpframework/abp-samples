834cc5c1fa901bfc1452bcfc6a6edc8620e387c8
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2020-11-17 15:13:26 +0300

    book to author server side relation

--- BookStore-Blazor-EfCore/test/Acme.BookStore.Application.Tests/Books/BookAppService_Tests.cs
+++ BookStore-Blazor-EfCore/test/Acme.BookStore.Application.Tests/Books/BookAppService_Tests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Threading.Tasks;
+using Acme.BookStore.Authors;
 using Shouldly;
 using Volo.Abp.Application.Dtos;
 using Volo.Abp.Validation;
@@ -11,10 +12,12 @@ namespace Acme.BookStore.Books
     public class BookAppService_Tests : BookStoreApplicationTestBase
     {
         private readonly IBookAppService _bookAppService;
+        private readonly IAuthorAppService _authorAppService;
 
         public BookAppService_Tests()
         {
             _bookAppService = GetRequiredService<IBookAppService>();
+            _authorAppService = GetRequiredService<IAuthorAppService>();
         }
 
         [Fact]
@@ -27,19 +30,24 @@ namespace Acme.BookStore.Books
 
             //Assert
             result.TotalCount.ShouldBeGreaterThan(0);
-            result.Items.ShouldContain(b => b.Name == "1984");
+            result.Items.ShouldContain(b => b.Name == "1984" &&
+                                       b.AuthorName == "George Orwell");
         }
 
         [Fact]
         public async Task Should_Create_A_Valid_Book()
         {
+            var authors = await _authorAppService.GetListAsync(new GetAuthorListDto());
+            var firstAuthor = authors.Items.First();
+
             //Act
             var result = await _bookAppService.CreateAsync(
                 new CreateUpdateBookDto
                 {
+                    AuthorId = firstAuthor.Id,
                     Name = "New test book 42",
                     Price = 10,
-                    PublishDate = DateTime.Now,
+                    PublishDate = System.DateTime.Now,
                     Type = BookType.ScienceFiction
                 }
             );
@@ -66,7 +74,7 @@ namespace Acme.BookStore.Books
             });
 
             exception.ValidationErrors
-                .ShouldContain(err => err.MemberNames.Any(mem => mem == "Name"));
+                .ShouldContain(err => err.MemberNames.Any(m => m == "Name"));
         }
     }
 }

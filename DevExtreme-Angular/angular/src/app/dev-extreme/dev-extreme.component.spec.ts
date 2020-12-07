b31ace532e59cdec673cdb351c3d1b0b50afe776
bnymncoskuner <bnymncoskuner@gmail.com>
2020-12-07 09:43:35 +0300

    Update angular to v11 in DevExtremeAngular

--- DevExtreme-Angular/angular/src/app/dev-extreme/dev-extreme.component.spec.ts
+++ DevExtreme-Angular/angular/src/app/dev-extreme/dev-extreme.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { DevExtremeComponent } from './dev-extreme.component';
 
@@ -6,7 +6,7 @@ describe('DevExtremeComponent', () => {
   let component: DevExtremeComponent;
   let fixture: ComponentFixture<DevExtremeComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ DevExtremeComponent ]
     })

1373317b4e4604b3f795d3556728101cd0d9a7b2
Yunus Emre Kalkan <emre.kalkan@volosoft.com>
2020-12-08 15:10:51 +0300

    Upgrade SignalRDemo to v4.0.0

--- SignalRDemo/src/SignalRDemo.EntityFrameworkCore.DbMigrations/Migrations/20201208120841_Upgraded_To_Abp_4_0_0.Designer.cs
+++ SignalRDemo/src/SignalRDemo.EntityFrameworkCore.DbMigrations/Migrations/20201208120841_Upgraded_To_Abp_4_0_0.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SignalRDemo.EntityFrameworkCore;
 using Volo.Abp.EntityFrameworkCore;
@@ -10,9 +11,10 @@ using Volo.Abp.EntityFrameworkCore;
 namespace SignalRDemo.Migrations
 {
     [DbContext(typeof(SignalRDemoMigrationsDbContext))]
-    partial class SignalRDemoMigrationsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201208120841_Upgraded_To_Abp_4_0_0")]
+    partial class Upgraded_To_Abp_4_0_0
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

93191e8402d75749ea5c3cf268e6cbd0c4f74c17
maliming <malimings@gmail.com>
2020-12-15 13:27:09 +0800

    Fix unit tests and Github CI.

--- build/common.ps1
+++ build/common.ps1
@@ -5,6 +5,7 @@ $rootFolder = (Get-Item -Path "./" -Verbose).FullName
 # List of solutions
 
 $solutionPaths = (
+    "../AcmeBookStoreAngularMaterial/aspnet-core",
     "../Authentication-Customization",
     "../BasicAspNetCoreApplication",
     "../BookStore-Angular-MongoDb/aspnet-core",
@@ -12,11 +13,15 @@ $solutionPaths = (
     "../BookStore-Modular/modules/book-management",
     "../BookStore-Modular/application",
     "../BookStore-Mvc-EfCore",
+    "../ClientSimulationDemo",
     "../DashboardDemo",
+    # "../DevExpress-Blazor",
 	# "../DevExtreme-Angular/aspnet-core",
 	# "../DevExtreme-Mvc",
     "../DocumentationSamples/CustomApplicationModules",
+    "../DocumentationSamples/ExtendLocalizationResource",
     "../EfCoreMigrationDemo",
+    "../EventOrganizer",
     "../GrpcDemo",
     "../KafkaEventBus",
     "../MicroserviceDemo",
@@ -24,6 +29,7 @@ $solutionPaths = (
 	"../PasswordlessAuthentication",
     "../PostgeSqlDemo",
     "../RabbitMqEventBus",
+    "../RebusEventBus",
     "../SignalRDemo",
     "../SignalRTieredDemo",
     "../StoredProcedureDemo",

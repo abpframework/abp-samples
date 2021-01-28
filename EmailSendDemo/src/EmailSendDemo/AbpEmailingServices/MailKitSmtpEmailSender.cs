50d7034261212ab919f1dfd15958a3ebebfcd990
maliming <malimings@gmail.com>
2021-01-28 10:16:41 +0800

    Add Email Send Demo.

---
+++ EmailSendDemo/src/EmailSendDemo/AbpEmailingServices/MailKitSmtpEmailSender.cs
@@ -0,0 +1,63 @@
+﻿using System.Net.Mail;
+using System.Threading.Tasks;
+using MailKit.Security;
+using Microsoft.Extensions.Options;
+using MimeKit;
+using SmtpClient = MailKit.Net.Smtp.SmtpClient;
+
+namespace EmailSendDemo.AbpEmailingServices
+{
+    public class MailKitSmtpEmailSender : EmailSenderBase
+    {
+        public MailKitSmtpEmailSender(IOptionsSnapshot<MailConfigOptions> settings)
+            : base(settings.Get("MailKit"))
+        {
+
+        }
+
+        protected override async Task SendEmailAsync(MailMessage mail)
+        {
+            using (var client = await BuildClientAsync())
+            {
+                var message = MimeMessage.CreateFromMailMessage(mail);
+                await client.SendAsync(message);
+                await client.DisconnectAsync(true);
+            }
+        }
+
+        public async Task<SmtpClient> BuildClientAsync()
+        {
+            var client = new SmtpClient();
+
+            try
+            {
+                await ConfigureClient(client);
+                return client;
+            }
+            catch
+            {
+                client.Dispose();
+                throw;
+            }
+        }
+
+        protected virtual async Task ConfigureClient(SmtpClient client)
+        {
+            await client.ConnectAsync(
+                ConfigOptions.Host,
+                ConfigOptions.Port,
+                SecureSocketOptions.SslOnConnect // or SecureSocketOptions.xx
+            );
+
+            if (ConfigOptions.UseDefaultCredentials)
+            {
+                return;
+            }
+
+            await client.AuthenticateAsync(
+                ConfigOptions.UserName,
+                ConfigOptions.Password
+            );
+        }
+    }
+}
---
+++ EmailSendDemo/src/EmailSendDemo/AbpEmailingServices/SmtpEmailSender.cs
@@ -0,0 +1,53 @@
+﻿using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+
+namespace EmailSendDemo.AbpEmailingServices
+{
+    public class SmtpEmailSender : EmailSenderBase
+    {
+        public SmtpEmailSender(IOptionsSnapshot<MailConfigOptions> settings)
+            : base(settings.Get("Smtp"))
+        {
+        }
+
+        public Task<SmtpClient> BuildClientAsync()
+        {
+            var host = ConfigOptions.Host;
+            var port = ConfigOptions.Port;
+
+            var smtpClient = new SmtpClient(host, port);
+
+            try
+            {
+                smtpClient.EnableSsl = ConfigOptions.EnableSsl;
+                smtpClient.UseDefaultCredentials = ConfigOptions.UseDefaultCredentials;
+                var userName = ConfigOptions.UserName;
+                if (!string.IsNullOrEmpty(userName))
+                {
+                    var password = ConfigOptions.Password;
+                    var domain = ConfigOptions.Domain;
+                    smtpClient.Credentials = !string.IsNullOrEmpty(domain)
+                        ? new NetworkCredential(userName, password, domain)
+                        : new NetworkCredential(userName, password);
+                }
+
+                return Task.FromResult(smtpClient);
+            }
+            catch
+            {
+                smtpClient.Dispose();
+                throw;
+            }
+        }
+
+        protected override async Task SendEmailAsync(MailMessage mail)
+        {
+            using (var smtpClient = await BuildClientAsync())
+            {
+                await smtpClient.SendMailAsync(mail);
+            }
+        }
+    }
+}

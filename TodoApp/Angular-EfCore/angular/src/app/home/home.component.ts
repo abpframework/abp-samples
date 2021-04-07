deebe9d312df1aafd132f249528b2aa9444b4ac2
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2021-04-07 14:48:33 +0300

    Implement angular UI for the todo app

--- TodoApp/Angular-EfCore/angular/src/app/home/home.component.ts
+++ TodoApp/Angular-EfCore/angular/src/app/home/home.component.ts
@@ -1,19 +1,36 @@
-import { AuthService } from '@abp/ng.core';
-import { Component } from '@angular/core';
-import { OAuthService } from 'angular-oauth2-oidc';
+import { ToasterService } from '@abp/ng.theme.shared';
+import { Component, OnInit } from '@angular/core';
+import { TodoItemDto, TodoService } from '@proxy';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
-  get hasLoggedIn(): boolean {
-    return this.oAuthService.hasValidAccessToken();
+export class HomeComponent implements OnInit {
+
+  todoItems: TodoItemDto[];
+  newTodoText: string;
+
+  constructor(private todoService: TodoService, private toasterService: ToasterService) {}
+
+  ngOnInit(): void {
+    this.todoService.getList().subscribe(response => {
+      this.todoItems = response;
+    });
+  }
+  
+  create(): void{
+    this.todoService.create(this.newTodoText).subscribe((result) => {
+      this.todoItems = this.todoItems.concat(result);
+      this.newTodoText = null;
+    });
   }
 
-  constructor(private oAuthService: OAuthService, private authService: AuthService) {}
-
-  login() {
-    this.authService.navigateToLogin();
-  }
+  delete(id: string): void {
+    this.todoService.delete(id).subscribe(() => {
+      this.todoItems = this.todoItems.filter(item => item.id !== id);
+      this.toasterService.info('Deleted the todo item.');
+    });
+  }  
 }

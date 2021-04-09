a4369ddf4bfc67735946c556bc6f395535bab9f0
liangshiwei <liangshiw@outlook.com>
2021-04-09 17:48:39 +0800

    Add blazor & MongoDB todo app

---
+++ TodoApp/BlazorWebAssembly-Mongo/src/TodoApp.Blazor/TodoAppBlazorAutoMapperProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace TodoApp.Blazor
+{
+    public class TodoAppBlazorAutoMapperProfile : Profile
+    {
+        public TodoAppBlazorAutoMapperProfile()
+        {
+            //Define your AutoMapper configuration here for the Blazor project.
+        }
+    }
+}
---
+++ TodoApp/BlazorWebAssembly-Mongo/src/TodoApp.Blazor/TodoAppBrandingProvider.cs
@@ -0,0 +1,11 @@
+﻿using Volo.Abp.DependencyInjection;
+using Volo.Abp.Ui.Branding;
+
+namespace TodoApp.Blazor
+{
+    [Dependency(ReplaceServices = true)]
+    public class TodoAppBrandingProvider : DefaultBrandingProvider
+    {
+        public override string AppName => "TodoApp";
+    }
+}

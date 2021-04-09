3d681f4281ecfe468a4faf3e870ba0100b549426
liangshiwei <liangshiw@outlook.com>
2021-04-09 17:55:24 +0800

    Add mvc & mongodb todo app

---
+++ TodoApp/Mvc-Mongo/src/TodoApp.Web/Menus/TodoAppMenus.cs
@@ -0,0 +1,11 @@
+﻿namespace TodoApp.Web.Menus
+{
+    public class TodoAppMenus
+    {
+        private const string Prefix = "TodoApp";
+        public const string Home = Prefix + ".Home";
+
+        //Add your menu items here...
+
+    }
+}
---
+++ TodoApp/Mvc-Mongo/test/TodoApp.Web.Tests/Pages/Index_Tests.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Shouldly;
+using Xunit;
+
+namespace TodoApp.Pages
+{
+    [Collection(TodoAppTestConsts.CollectionDefinitionName)]
+    public class Index_Tests : TodoAppWebTestBase
+    {
+        [Fact]
+        public async Task Welcome_Page()
+        {
+            var response = await GetResponseAsStringAsync("/");
+            response.ShouldNotBeNull();
+        }
+    }
+}

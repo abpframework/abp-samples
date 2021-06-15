0b930e02167b5c742e4e7521a8f28d4efe3c8948
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2021-06-15 17:42:13 +0300

    Removed ProductEditViewModel usage.

--- ConcurrencyDemo/src/ConcurrencyDemo.Web/Pages/ProductEdit.cshtml.cs
+++ ConcurrencyDemo/src/ConcurrencyDemo.Web/Pages/ProductEdit.cshtml.cs
@@ -9,8 +9,8 @@ namespace ConcurrencyDemo.Web.Pages
     public class ProductEdit : PageModel
     {
         [BindProperty]
-        public ProductEditViewModel Product { get; set; }
-        
+        public ProductDto Product { get; set; }
+
         private readonly IProductAppService _productAppService;
 
         public ProductEdit(IProductAppService productAppService)
@@ -20,40 +20,13 @@ namespace ConcurrencyDemo.Web.Pages
         
         public async Task OnGetAsync(Guid id)
         {
-            var productDto = await _productAppService.GetAsync(id);
-            Product = new ProductEditViewModel
-            {
-                Id = productDto.Id,
-                Name = productDto.Name,
-                Price = productDto.Price,
-                ConcurrencyStamp = productDto.ConcurrencyStamp
-            };
+            Product = await _productAppService.GetAsync(id);
         }
 
         public async Task<IActionResult> OnPostAsync()
         {
-            await _productAppService.UpdateAsync(
-                new ProductDto
-                {
-                    Id = Product.Id,
-                    Name = Product.Name,
-                    Price = Product.Price,
-                    ConcurrencyStamp = Product.ConcurrencyStamp
-                });
-            
+            await _productAppService.UpdateAsync(Product);
             return RedirectToPage("/");
-        }
-        
-        public class ProductEditViewModel : IHasConcurrencyStamp
-        {
-            [HiddenInput]
-            public Guid Id { get; set; }
-            
-            public string Name { get; set; }
-            public float Price { get; set; }
-            
-            [HiddenInput]
-            public string ConcurrencyStamp { get; set; }
         }
     }
 }

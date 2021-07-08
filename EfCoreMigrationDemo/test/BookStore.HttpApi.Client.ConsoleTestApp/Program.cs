9a67a398e04492f618a61d1c0771b647362ccc04
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2021-07-08 13:14:09 +0300

    Replace EfCoreMigrationDemo with an empty project

---
+++ EfCoreMigrationDemo/test/BookStore.HttpApi.Client.ConsoleTestApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace BookStore.HttpApi.Client.ConsoleTestApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await CreateHostBuilder(args).RunConsoleAsync();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(build =>
+                {
+                    build.AddJsonFile("appsettings.secrets.json", optional: true);
+                })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<ConsoleTestAppHostedService>();
+                });
+    }
+}

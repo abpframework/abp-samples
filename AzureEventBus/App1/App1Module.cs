6235d2d6707bf3ac1a2574f720390c5e3da9e4f1
maliming <malimings@gmail.com>
2021-07-13 15:00:47 +0800

    Add AzureEventBus demo.
    https://github.com/abpframework/abp/pull/8375

---
+++ AzureEventBus/App1/App1Module.cs
@@ -0,0 +1,29 @@
+﻿using Volo.Abp.Autofac;
+using Volo.Abp.EventBus.Azure;
+using Volo.Abp.AzureServiceBus;
+using Volo.Abp.Modularity;
+
+namespace App1
+{
+    [DependsOn(
+        typeof(AbpEventBusAzureModule),
+        typeof(AbpAutofacModule)
+        )]
+    public class App1Module : AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            Configure<AbpAzureEventBusOptions>(options =>
+            {
+                options.ConnectionName = "Default";
+                options.SubscriberName = "MySubscriberName";
+                options.TopicName = "MyTopicName";
+            });
+
+            Configure<AbpAzureServiceBusOptions>(options =>
+            {
+                options.Connections.Default.ConnectionString = "Endpoint=sb://testabp.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=koFvICQA43KZ0yLfPHg0yPujh47GhJZ9fLIfm/7ePqw=";
+            });
+        }
+    }
+}

6bf903eaa73802b89ca2ceb80804302c7a26fea4
Enis Necipoglu <enis.necipoglu@gmail.com>
2022-03-30 14:30:36 +0300

    Redirect Account/manage to keycloak

--- KeycloakDemo/KeycloakDemo-Tiered-Mvc/src/KeycloakDemo.Web/Menus/KeycloakDemoMenuContributor.cs
+++ KeycloakDemo/KeycloakDemo-Tiered-Mvc/src/KeycloakDemo.Web/Menus/KeycloakDemoMenuContributor.cs
@@ -73,7 +73,7 @@ public class KeycloakDemoMenuContributor : IMenuContributor
         var identityServerUrl = _configuration["AuthServer:Authority"] ?? "";
 
         context.Menu.AddItem(new ApplicationMenuItem("Account.Manage", accountStringLocalizer["MyAccount"],
-            $"{identityServerUrl.EnsureEndsWith('/')}Account/Manage?returnUrl={_configuration["App:SelfUrl"]}", icon: "fa fa-cog", order: 1000, null, "_blank").RequireAuthenticated());
+            $"{identityServerUrl.EnsureEndsWith('/')}account/#/personal-info", icon: "fa fa-cog", order: 1000, null, "_blank").RequireAuthenticated());
         context.Menu.AddItem(new ApplicationMenuItem("Account.Logout", l["Logout"], url: "~/Account/Logout", icon: "fa fa-power-off", order: int.MaxValue - 1000).RequireAuthenticated());
 
         return Task.CompletedTask;

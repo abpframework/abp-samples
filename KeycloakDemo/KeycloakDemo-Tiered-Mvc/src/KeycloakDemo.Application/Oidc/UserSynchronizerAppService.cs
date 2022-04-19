1977e591cf6209e5d23587bf5a98936c7860a937
enisn <info@enisnecipoglu.com>
2022-04-19 11:20:24 +0300

    Add simple sync logic

--- KeycloakDemo/KeycloakDemo-Tiered-Mvc/src/KeycloakDemo.Application/Oidc/UserSynchronizerAppService.cs
+++ KeycloakDemo/KeycloakDemo-Tiered-Mvc/src/KeycloakDemo.Application/Oidc/UserSynchronizerAppService.cs
@@ -1,17 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Volo.Abp.Identity;
 
 namespace KeycloakDemo.Oidc;
 
 public class UserSynchronizerAppService : KeycloakDemoAppService, IUserSynchronizerAppService
 {
-    public Task SyncAsync(SyncInputDto input)
-    {
-        var currentUserClaims = CurrentUser.GetAllClaims();
+    protected readonly IdentityUserManager userManager;
 
-        return Task.CompletedTask;
+    public UserSynchronizerAppService(IdentityUserManager userManager)
+    {
+        this.userManager = userManager;
+    }
+
+    public async Task SyncAsync(SyncInputDto input)
+    {
+        var id = Guid.Parse(input.Claims.Find(x => x.Type == ClaimTypes.NameIdentifier).Value);
+        var userName = input.Claims.Find(x => x.Type == "preferred_username")?.Value;
+        var email = input.Claims.Find(x => x.Type == ClaimTypes.Email)?.Value;
+
+        var user = new IdentityUser(id, userName, email, CurrentTenant.Id);
+
+        await userManager.CreateAsync(user);
     }
 }

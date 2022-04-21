5970d6971c30badc655abe70a4d99ed6416b0026
enisn <info@enisnecipoglu.com>
2022-04-21 11:44:42 +0300

    Remove Sync services

--- KeycloakDemo/KeycloakDemo-Tiered-Mvc/src/KeycloakDemo.HttpApi.Client/ClientProxies/UserSynchronizerClientProxy.cs
+++
@@ -1,7 +0,0 @@
-// This file is part of UserSynchronizerClientProxy, you can customize it here
-// ReSharper disable once CheckNamespace
-namespace KeycloakDemo.Oidc.ClientProxies;
-
-public partial class UserSynchronizerClientProxy
-{
-}

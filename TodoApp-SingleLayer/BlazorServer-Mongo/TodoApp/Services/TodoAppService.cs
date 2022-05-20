8a2fe2d7b3db51c65612fb906d42c0f6d7d06d53
Engincan VESKE <enginveske@gmail.com>
2022-05-20 15:21:06 +0300

    Create sample BlazorServer-Mongo todo app

---
+++ TodoApp-SingleLayer/BlazorServer-Mongo/TodoApp/Services/TodoAppService.cs
@@ -0,0 +1,45 @@
+﻿using TodoApp.Entities;
+using TodoApp.Services.Dtos;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Domain.Repositories;
+
+namespace TodoApp.Services;
+
+public class TodoAppService : ApplicationService
+{
+    private readonly IRepository<TodoItem, Guid> _todoItemRepository;
+    
+    public TodoAppService(IRepository<TodoItem, Guid> todoItemRepository)
+    {
+        _todoItemRepository = todoItemRepository;
+    }
+    
+    public async Task<List<TodoItemDto>> GetListAsync()
+    {
+        var items = await _todoItemRepository.GetListAsync();
+        return items
+            .Select(item => new TodoItemDto
+            {
+                Id = item.Id,
+                Text = item.Text
+            }).ToList();
+    }
+    
+    public async Task<TodoItemDto> CreateAsync(string text)
+    {
+        var todoItem = await _todoItemRepository.InsertAsync(
+            new TodoItem {Text = text}
+        );
+
+        return new TodoItemDto
+        {
+            Id = todoItem.Id,
+            Text = todoItem.Text
+        };
+    }
+    
+    public async Task DeleteAsync(Guid id)
+    {
+        await _todoItemRepository.DeleteAsync(id);
+    }
+}

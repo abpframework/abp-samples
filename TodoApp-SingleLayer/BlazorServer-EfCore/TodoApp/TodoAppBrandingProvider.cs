9fc441af6c264969d7fcaa4dd20358811a09ac8c
Engincan VESKE <enginveske@gmail.com>
2022-05-20 15:08:34 +0300

    Create sample BlazorServer-EfCore todo app

---
+++ TodoApp-SingleLayer/BlazorServer-EfCore/TodoApp/TodoAppBrandingProvider.cs
@@ -0,0 +1,10 @@
+﻿using Volo.Abp.DependencyInjection;
+using Volo.Abp.Ui.Branding;
+
+namespace TodoApp;
+
+[Dependency(ReplaceServices = true)]
+public class TodoAppBrandingProvider : DefaultBrandingProvider
+{
+    public override string AppName => "TodoApp";
+}
---
+++ TodoApp-SingleLayer/BlazorServer-EfCore/TodoApp/TodoAppComponentBase.cs
@@ -0,0 +1,12 @@
+﻿using TodoApp.Localization;
+using Volo.Abp.AspNetCore.Components;
+
+namespace TodoApp;
+
+public abstract class TodoAppComponentBase : AbpComponentBase
+{
+    protected TodoAppComponentBase()
+    {
+        LocalizationResource = typeof(TodoAppResource);
+    }
+}

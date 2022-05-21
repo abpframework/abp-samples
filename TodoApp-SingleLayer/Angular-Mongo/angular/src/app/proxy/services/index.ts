2d8118c5fc403c23cf4c091742044bc872c4c8cc
Engincan VESKE <enginveske@gmail.com>
2022-05-21 11:39:23 +0300

    Create sample Angular-Mongo todo app (single-layer)

---
+++ TodoApp-SingleLayer/Angular-Mongo/angular/src/app/proxy/services/index.ts
@@ -0,0 +1,3 @@
+import * as Dtos from './dtos';
+export * from './todo.service';
+export { Dtos };
---
+++ TodoApp-SingleLayer/Angular-Mongo/angular/src/environments/environment.ts
@@ -0,0 +1,26 @@
+import { Environment } from '@abp/ng.core';
+
+const baseUrl = 'http://localhost:4200';
+
+export const environment = {
+  production: false,
+  application: {
+    baseUrl,
+    name: 'TodoApp',
+    logoUrl: '',
+  },
+  oAuthConfig: {
+    issuer: 'https://localhost:44350',
+    redirectUri: baseUrl,
+    clientId: 'TodoApp_App',
+    responseType: 'code',
+    scope: 'offline_access TodoApp',
+    requireHttps: true,
+  },
+  apis: {
+    default: {
+      url: 'https://localhost:44350',
+      rootNamespace: 'TodoApp',
+    },
+  },
+} as Environment;

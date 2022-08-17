80e943618c3339060ae04496b062a8200ffb7b75
Salih <salihozkara00@gmail.com>
2022-08-17 15:21:39 +0300

    Updated BookStore-Modular to 6.0.0-rc.2

--- BookStore-Modular/application/src/Acme.BookStore.Domain/Users/AppUser.cs
+++ BookStore-Modular/application/src/Acme.BookStore.Domain/Users/AppUser.cs
@@ -38,6 +38,8 @@ namespace Acme.BookStore.Users
 
         public virtual bool PhoneNumberConfirmed { get; private set; }
 
+        public virtual bool IsActive { get; private set; }
+
         #endregion
 
         /* Add your own properties here. Example:

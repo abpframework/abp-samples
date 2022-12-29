7f12e080845ec20577cac43f2fe65df19a906c49
malik masis <malik.masis@gmail.com>
2022-12-29 17:37:25 +0300

    Completed Part 2: The Book List Page

--- BookStore-Blazor-EfCore/src/Acme.BookStore.Blazor/Menus/BookStoreMenuContributor.cs
+++ BookStore-Blazor-EfCore/src/Acme.BookStore.Blazor/Menus/BookStoreMenuContributor.cs
@@ -34,6 +34,21 @@ public class BookStoreMenuContributor : IMenuContributor
             )
         );
 
+        context.Menu.AddItem(
+            new ApplicationMenuItem(
+                "BooksStore",
+                l["Menu:BookStore"],
+                icon: "fa fa-book"
+            ).AddItem(
+                new ApplicationMenuItem(
+                    "BooksStore.Books",
+                    l["Menu:Books"],
+                    url: "/books"
+                )
+            )
+        );
+
+
         if (MultiTenancyConsts.IsEnabled)
         {
             administration.SetSubItemOrder(TenantManagementMenuNames.GroupName, 1);

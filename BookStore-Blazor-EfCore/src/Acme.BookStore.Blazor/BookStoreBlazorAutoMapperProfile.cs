7f70f5341b9e59db5fe22a123c9e9d0f1fb1912f
malik masis <malik.masis@gmail.com>
2022-12-29 18:07:48 +0300

    Completed - Part 3: Creating, Updating and Deleting Books

---
+++ BookStore-Blazor-EfCore/src/Acme.BookStore.Blazor/BookStoreBlazorAutoMapperProfile.cs
@@ -0,0 +1,12 @@
+﻿using Acme.BookStore.Books;
+using AutoMapper;
+
+namespace Acme.BookStore.Blazor;
+
+public class BookStoreBlazorAutoMapperProfile : Profile
+{
+    public BookStoreBlazorAutoMapperProfile()
+    {
+        CreateMap<BookDto, CreateUpdateBookDto>();
+    }
+}

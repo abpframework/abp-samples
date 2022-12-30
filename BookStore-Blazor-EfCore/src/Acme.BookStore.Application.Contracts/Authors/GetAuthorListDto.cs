333fa745f7c5fcc3c097d4b69ed4be5d91afa3d4
malik masis <malik.masis@gmail.com>
2022-12-30 11:25:49 +0300

    Completed - Part 8: Authors: Application Layer

---
+++ BookStore-Blazor-EfCore/src/Acme.BookStore.Application.Contracts/Authors/GetAuthorListDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Volo.Abp.Application.Dtos;
+
+namespace Acme.BookStore.Authors;
+
+[Serializable]
+public class GetAuthorListDto : PagedAndSortedResultRequestDto
+{
+    public string Filter { get; set; }
+}
--- BookStore-Blazor-EfCore/src/Acme.BookStore.Application/BookStoreApplicationAutoMapperProfile.cs
+++ BookStore-Blazor-EfCore/src/Acme.BookStore.Application/BookStoreApplicationAutoMapperProfile.cs
@@ -1,4 +1,5 @@
-﻿using Acme.BookStore.Books;
+﻿using Acme.BookStore.Authors;
+using Acme.BookStore.Books;
 using AutoMapper;
 
 namespace Acme.BookStore;
@@ -7,11 +8,8 @@ public class BookStoreApplicationAutoMapperProfile : Profile
 {
     public BookStoreApplicationAutoMapperProfile()
     {
-        /* You can configure your AutoMapper mapping configuration here.
-         * Alternatively, you can split your mapping configurations
-         * into multiple profile classes for a better organization. */
-
         CreateMap<Book, BookDto>();
         CreateMap<CreateUpdateBookDto, Book>();
+        CreateMap<Author, AuthorDto>();
     }
 }

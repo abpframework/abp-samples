540813ce8bf331d179a0730182664858f7a5ca75
malik masis <malik.masis@gmail.com>
2022-12-30 11:15:05 +0300

    Completed - Part 7: Authors: Database Integration

--- BookStore-Blazor-EfCore/src/Acme.BookStore.EntityFrameworkCore/Migrations/20221230081214_Added_Authors.Designer.cs
+++ BookStore-Blazor-EfCore/src/Acme.BookStore.EntityFrameworkCore/Migrations/20221230081214_Added_Authors.Designer.cs
@@ -4,6 +4,7 @@ using Acme.BookStore.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Volo.Abp.EntityFrameworkCore;
 
@@ -12,9 +13,11 @@ using Volo.Abp.EntityFrameworkCore;
 namespace Acme.BookStore.Migrations
 {
     [DbContext(typeof(BookStoreDbContext))]
-    partial class BookStoreDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221230081214_Added_Authors")]
+    partial class AddedAuthors
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

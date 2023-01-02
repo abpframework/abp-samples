2fc30c54eb7e586ecd5724636ac44084014ff47c
malik masis <malik.masis@gmail.com>
2023-01-02 20:19:53 +0300

    completed part8

---
+++ BookStore-Angular-MongoDb/aspnet-core/src/Acme.BookStore.Application.Contracts/Authors/UpdateAuthorDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Acme.BookStore.Authors;
+
+public class UpdateAuthorDto
+{
+    [Required]
+    [StringLength(AuthorConsts.MaxNameLength)]
+    public string Name { get; set; }
+
+    [Required]
+    public DateTime BirthDate { get; set; }
+
+    public string ShortBio { get; set; }
+}
--- BookStore-Angular-MongoDb/aspnet-core/src/Acme.BookStore.Application.Contracts/Permissions/BookStorePermissions.cs
+++ BookStore-Angular-MongoDb/aspnet-core/src/Acme.BookStore.Application.Contracts/Permissions/BookStorePermissions.cs
@@ -11,4 +11,12 @@ public static class BookStorePermissions
         public const string Edit = Default + ".Edit";
         public const string Delete = Default + ".Delete";
     }
+
+    public static class Authors
+    {
+        public const string Default = GroupName + ".Authors";
+        public const string Create = Default + ".Create";
+        public const string Edit = Default + ".Edit";
+        public const string Delete = Default + ".Delete";
+    }
 }

35048581b7723abb9648b68589e9caa0ad80392c
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2023-01-03 11:54:10 +0300

    Example services

---
+++ BookStore-Mvc-EfCore/src/Acme.BookStore.Domain/Books/BookManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading.Tasks;
+using Volo.Abp.Data;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.Domain.Repositories;
+using Volo.Abp.MultiTenancy;
+
+namespace Acme.BookStore.Books;
+
+    public class BookService : ITransientDependency
+    {
+        private readonly IRepository<Book, Guid> _bookRepository;
+        private readonly ICurrentTenant _currentTenant;
+
+        public BookService(IRepository<Book, Guid> bookRepository, ICurrentTenant currentTenant)
+        {
+            _bookRepository = bookRepository;
+            _currentTenant = currentTenant;
+        }
+
+        public async Task<long> GetTenantBookCountAsync(Guid? tenantId)
+        {
+            using (_currentTenant.Change(tenantId))
+            {
+                return await _bookRepository.GetCountAsync();
+            }
+        }
+    }
+    
+    public class BookService2 : ITransientDependency
+    {
+        private readonly IRepository<Book, Guid> _bookRepository;
+        private readonly IDataFilter _dataFilter;
+
+        public BookService2(IRepository<Book, Guid> bookRepository, IDataFilter dataFilter)
+        {
+            _bookRepository = bookRepository;
+            _dataFilter = dataFilter;
+        }
+
+        public async Task<long> GetTotalBookCountAsync()
+        {
+            using (_dataFilter.Disable<IMultiTenant>())
+            {
+                return await _bookRepository.GetCountAsync();
+            }
+        }
+    }

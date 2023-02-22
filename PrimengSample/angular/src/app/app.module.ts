b50584ab8ca720bd5414540d8b9ddd0f9e956994
Barış Can Yılmaz <30300440+bariscanyilmaz@users.noreply.github.com>
2023-02-22 10:27:42 +0300

    migrate from tslint to eslint and use leptonx lite

--- PrimengSample/angular/src/app/app.module.ts
+++ PrimengSample/angular/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { registerLocale } from '@abp/ng.core/locale';
 import { IdentityConfigModule } from '@abp/ng.identity/config';
 import { SettingManagementConfigModule } from '@abp/ng.setting-management/config';
 import { TenantManagementConfigModule } from '@abp/ng.tenant-management/config';
-import { ThemeBasicModule } from '@abp/ng.theme.basic';
 import { ThemeSharedModule } from '@abp/ng.theme.shared';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
@@ -15,6 +14,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { APP_ROUTE_PROVIDER } from './route.provider';
 import { AbpOAuthModule } from '@abp/ng.oauth';
+import { ThemeLeptonXModule } from '@abp/ng.theme.lepton-x';
+import { SideMenuLayoutModule } from '@abp/ng.theme.lepton-x/layouts';
 
 @NgModule({
   imports: [
@@ -30,7 +31,8 @@ import { AbpOAuthModule } from '@abp/ng.oauth';
     TenantManagementConfigModule.forRoot(),
     SettingManagementConfigModule.forRoot(),
     AccountConfigModule.forRoot(),
-    ThemeBasicModule.forRoot(),
+    ThemeLeptonXModule.forRoot(),
+    SideMenuLayoutModule.forRoot(),
     AbpOAuthModule.forRoot(),
   ],
   declarations: [AppComponent],

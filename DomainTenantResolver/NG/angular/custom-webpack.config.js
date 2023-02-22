7dbdd4e221fef37ed556ea1a99cdfc6d74f58408
Barış Can Yılmaz <30300440+bariscanyilmaz@users.noreply.github.com>
2023-02-22 18:39:48 +0300

    domaina tenant resolver ng upgrade done

--- DomainTenantResolver/NG/angular/custom-webpack.config.js
+++ DomainTenantResolver/NG/angular/custom-webpack.config.js
@@ -1,6 +1,9 @@
-module.exports = (config) => {
-    var path = require('path');
-    var pachArr = __dirname.split(path.sep);
-    config.devServer.pfx = pachArr.slice(0, pachArr.length - 2).join(path.sep) + path.sep + 'getabp.net.pfx';
-    return config;
+var path = require('path');
+var pachArr = __dirname.split(path.sep);
+module.exports = {
+  devServer: {
+    https: {
+      pfx: pachArr.slice(0, pachArr.length - 2).join(path.sep) + path.sep + 'getabp.net.pfx',
+    },
+  },
 };

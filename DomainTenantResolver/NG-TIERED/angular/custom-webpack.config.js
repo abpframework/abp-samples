67257b6e11953237a92cc7d67c266fb64624fed3
Barış Can Yılmaz <30300440+bariscanyilmaz@users.noreply.github.com>
2023-02-24 15:22:38 +0300

    domain tenant resolver ng-tiered update done

--- DomainTenantResolver/NG-TIERED/angular/custom-webpack.config.js
+++ DomainTenantResolver/NG-TIERED/angular/custom-webpack.config.js
@@ -1,9 +1,14 @@
 var path = require('path');
 var pachArr = __dirname.split(path.sep);
+const fs = require('fs');
+
 module.exports = {
   devServer: {
-    https: {
-      pfx: pachArr.slice(0, pachArr.length - 2).join(path.sep) + path.sep + 'getabp.net.pfx',
-    },
-  },
-};
+    server : {
+      type: 'https',
+      options: {
+        pfx: fs.readFileSync(pachArr.slice(0, pachArr.length - 3).join(path.sep) + path.sep + 'getabp.net.pfx')
+      }
+    }
+  }
+}

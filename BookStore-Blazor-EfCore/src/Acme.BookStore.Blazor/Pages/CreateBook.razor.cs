aae61ad6d66ebf6191dd4dcfb4e23d30bd680a4e
Enis Necipoglu <iletisim@enisnecipoglu.com>
2023-03-28 16:43:49 +0300

    Convert Create/Edit modals to page

---
+++ BookStore-Blazor-EfCore/src/Acme.BookStore.Blazor/Pages/CreateBook.razor.cs
@@ -0,0 +1,55 @@
+﻿using Acme.BookStore.Books;
+using Blazorise;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Volo.Abp;
+
+namespace Acme.BookStore.Blazor.Pages;
+
+public partial class CreateBook
+{
+    protected Validations CreateValidationsRef;
+    protected CreateUpdateBookDto NewEntity = new();
+    IReadOnlyList<AuthorLookupDto> authorList = Array.Empty<AuthorLookupDto>();
+
+    protected override async Task OnInitializedAsync()
+    {
+        await base.OnInitializedAsync();
+        authorList = (await AppService.GetAuthorLookupAsync()).Items;
+
+        if (!authorList.Any())
+        {
+            throw new UserFriendlyException(message: L["AnAuthorIsRequiredForCreatingBook"]);
+        }
+
+        NewEntity.AuthorId = authorList.First().Id;
+
+        if (CreateValidationsRef != null)
+        {
+            await CreateValidationsRef.ClearAll();
+        }
+    }
+
+    protected virtual async Task CreateEntityAsync()
+    {
+        try
+        {
+            var validate = true;
+            if (CreateValidationsRef != null)
+            {
+                validate = await CreateValidationsRef.ValidateAll();
+            }
+            if (validate)
+            {
+                await AppService.CreateAsync(NewEntity);
+                NavigationManager.NavigateTo("books");
+            }
+        }
+        catch (Exception ex)
+        {
+            await HandleErrorAsync(ex);
+        }
+    }
+}
---
+++ BookStore-Blazor-EfCore/src/Acme.BookStore.Blazor/Pages/EditBook.razor.cs
@@ -0,0 +1,69 @@
+﻿using Acme.BookStore.Books;
+using Blazorise;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Volo.Abp;
+
+namespace Acme.BookStore.Blazor.Pages;
+
+public partial class EditBook
+{
+    protected CreateUpdateBookDto EditingEntity = new();
+    protected Validations EditValidationsRef;
+    IReadOnlyList<AuthorLookupDto> authorList = Array.Empty<AuthorLookupDto>();
+
+    [Parameter]
+    public string Id { get; set; }
+
+    public Guid EditingEntityId { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        await base.OnInitializedAsync();
+
+        // Blazor can't parse Guid as route constraint currently.
+        // See https://github.com/dotnet/aspnetcore/issues/19008
+        EditingEntityId = Guid.Parse(Id);
+
+        authorList = (await AppService.GetAuthorLookupAsync()).Items;
+
+        if (!authorList.Any())
+        {
+            throw new UserFriendlyException(message: L["AnAuthorIsRequiredForCreatingBook"]);
+        }
+
+        var entityDto = await AppService.GetAsync(EditingEntityId);
+
+        EditingEntity = ObjectMapper.Map<BookDto,CreateUpdateBookDto>(entityDto);
+
+        if (EditValidationsRef != null)
+        {
+            await EditValidationsRef.ClearAll();
+        }
+    }
+
+    protected virtual async Task UpdateEntityAsync()
+    {
+        try
+        {
+            var validate = true;
+            if (EditValidationsRef != null)
+            {
+                validate = await EditValidationsRef.ValidateAll();
+            }
+            if (validate)
+            {
+                await AppService.UpdateAsync(EditingEntityId, EditingEntity);
+
+                NavigationManager.NavigateTo("books");
+            }
+        }
+        catch (Exception ex)
+        {
+            await HandleErrorAsync(ex);
+        }
+    }
+}

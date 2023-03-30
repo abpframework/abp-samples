b0fbd634869e7d355370a8417f175ecbf59bd32b
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2023-03-30 03:16:25 +0300

    Show books on UI

--- Dapper/DapperDemo/Pages/Index.cshtml.cs
+++ Dapper/DapperDemo/Pages/Index.cshtml.cs
@@ -1,8 +1,22 @@
-﻿using Volo.Abp.AspNetCore.Mvc.UI.RazorPages;
+﻿using DapperDemo.DataViews;
+using DapperDemo.Repositories;
+using Volo.Abp.AspNetCore.Mvc.UI.RazorPages;
 
 namespace DapperDemo.Pages;
 
 public class IndexModel : AbpPageModel
 {
+    public List<BookDataView> Books { get; private set; }
+
+    private readonly IBookRepository _bookRepository;
+
+    public IndexModel(IBookRepository bookRepository)
+    {
+        _bookRepository = bookRepository;
+    }
     
+    public async Task OnGetAsync()
+    {
+        Books = await _bookRepository.GetListAsync();
+    }
 }

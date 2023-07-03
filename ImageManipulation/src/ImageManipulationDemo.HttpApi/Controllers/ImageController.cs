953e8e9c33b82b74a4705273df913e10a69269c3
Engincan VESKE <43685404+EngincanV@users.noreply.github.com>
2023-07-03 12:10:27 +0300

    image upload with resize & compress for ImageManipulationDemo

---
+++ ImageManipulation/src/ImageManipulationDemo.HttpApi/Controllers/ImageController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Volo.Abp.BlobStoring;
+using Volo.Abp.Imaging;
+
+namespace ImageManipulationDemo.Controllers
+{
+    [Controller]
+    [Route("api/image")]
+    public class ImageController : ImageManipulationDemoController
+    {
+        private readonly IBlobContainer<ImageManipulationContainer> _blobContainer;
+
+        public ImageController(IBlobContainer<ImageManipulationContainer> blobContainer)
+        {
+            _blobContainer = blobContainer;
+        }
+
+        [HttpPost("upload")]
+        [CompressImage]
+        [ResizeImage(width: 100, height: 100)]
+        public async Task<IActionResult> UploadAsync(IFormFile file)
+        {
+            var fileBytes = await file.GetAllBytesAsync();
+            var blobName = file.FileName;
+
+            await _blobContainer.SaveAsync(blobName, fileBytes);
+
+            return Ok();
+        }
+
+        [HttpGet("")]
+        public async Task<byte[]> GetImageAsync(string fileName)
+        {
+            return await _blobContainer.GetAllBytesAsync(fileName);
+        }
+    }
+}
---
+++ ImageManipulation/src/ImageManipulationDemo.HttpApi/ImageManipulationContainer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.BlobStoring;
+
+namespace ImageManipulationDemo
+{
+    [BlobContainerName("image-manipulation-demo")]
+    public class ImageManipulationContainer
+    {
+    }
+}

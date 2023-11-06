2e5646d459c996a2e26dd853f9d2b627318e8f2b
Kori Francis <kfrancis@clinicalsupportsystems.com>
2023-11-06 08:55:05 -0500

    add user/tenant args for #254

--- SeparateBackgroundJob/Shared/SeparateBackgroundJob.Common.Shared/MyReportJob.cs
+++ SeparateBackgroundJob/Shared/SeparateBackgroundJob.Common.Shared/MyReportJob.cs
@@ -1,14 +1,50 @@
 using Microsoft.Extensions.Logging;
+using System.Security.Claims;
 using Volo.Abp.BackgroundJobs;
 using Volo.Abp.DependencyInjection;
+using Volo.Abp.MultiTenancy;
+using Volo.Abp.Security.Claims;
+using Volo.Abp.Uow;
+using Volo.Abp.Users;
 
 namespace SeparateBackgroundJob.Common.Shared;
 
 public class MyReportJob : AsyncBackgroundJob<MyReportJobArgs>, ITransientDependency
 {
+    private readonly ICurrentPrincipalAccessor _currentPrincipalAccessor;
+
+    public MyReportJob(ICurrentTenant currentTenant, ICurrentPrincipalAccessor currentPrincipalAccessor)
+    {
+        CurrentTenant = currentTenant;
+        _currentPrincipalAccessor = currentPrincipalAccessor;
+    }
+
+    private ICurrentTenant CurrentTenant { get; }
+
+    [UnitOfWork]
     public override Task ExecuteAsync(MyReportJobArgs args)
     {
         Logger.LogInformation("Executing MyReportJob with args: {0}", args.Content);
+
+        using (var scope = _currentPrincipalAccessor.Change(CreatePrincipal(args.UserId, args.TenantId)))
+        {
+            // Your code here, the user context has been changed.
+            Logger.LogInformation("Executing MyReportJob as tenant/user: {0}/{1}", args.TenantId, args.UserId);
+        }
+
         return Task.CompletedTask;
+    }
+
+    private ClaimsPrincipal CreatePrincipal(Guid userId, Guid? tenantId)
+    {
+        var identity = new ClaimsIdentity();
+        identity.AddClaim(new Claim(AbpClaimTypes.UserId, userId.ToString()));
+
+        if (tenantId.HasValue)
+        {
+            identity.AddClaim(new Claim(AbpClaimTypes.TenantId, tenantId.Value.ToString()));
+        }
+
+        return new ClaimsPrincipal(identity);
     }
 }
--- SeparateBackgroundJob/Shared/SeparateBackgroundJob.Common.Shared/MyReportJobArgs.cs
+++ SeparateBackgroundJob/Shared/SeparateBackgroundJob.Common.Shared/MyReportJobArgs.cs
@@ -3,4 +3,6 @@ namespace SeparateBackgroundJob.Common.Shared;
 public class MyReportJobArgs
 {
     public string? Content { get; set; }
+    public Guid UserId { get; set; }
+    public Guid? TenantId { get; set; }
 }

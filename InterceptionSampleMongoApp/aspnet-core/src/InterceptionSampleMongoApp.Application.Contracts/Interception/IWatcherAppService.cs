c8730ef3b715024095ef5f889ec0854eb4ac6b6f
braim23 <94292623+braim23@users.noreply.github.com>
2023-11-09 15:54:36 +0300

    Added Interception Code

---
+++ InterceptionSampleMongoApp/aspnet-core/src/InterceptionSampleMongoApp.Application.Contracts/Interception/IWatcherAppService.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using Volo.Abp.Application.Services;
+
+namespace InterceptionSampleMongoApp.Interception;
+
+public interface IWatcherAppService : IApplicationService
+{
+    Task<int> Method1Async();
+    Task<int> Method2Async();
+    Task Method3Async();
+    Task Method4Async();
+}
---
+++ InterceptionSampleMongoApp/aspnet-core/src/InterceptionSampleMongoApp.Application/Interception/WatcherAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace InterceptionSampleMongoApp.Interception;
+
+[AttributeUsage(AttributeTargets.Method | AttributeTargets.Class)]
+public class WatcherAttribute : Attribute
+{
+    public bool IsDisabled { get; set; }
+
+    public TimeSpan Timeout { get; }
+
+    public WatcherAttribute(int second = 5, int minute = 0, int hour = 0)
+    {
+        Timeout = new TimeSpan(hour, minute, second);
+    }
+}

58cafb83261173781261d9c99414701759ae6d81
Galip Tolga Erdem <galiperdem@gmail.com>
2024-01-04 15:11:25 -0500

    Added v8 migrations

--- GrpcDemo/src/GrpcDemo.EntityFrameworkCore.DbMigrations/Migrations/20240104201038_Updated_To_v8.Designer.cs
+++ GrpcDemo/src/GrpcDemo.EntityFrameworkCore.DbMigrations/Migrations/20240104201038_Updated_To_v8.Designer.cs
@@ -4,6 +4,7 @@ using GrpcDemo.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Volo.Abp.EntityFrameworkCore;
 
@@ -12,9 +13,11 @@ using Volo.Abp.EntityFrameworkCore;
 namespace GrpcDemo.Migrations
 {
     [DbContext(typeof(GrpcDemoMigrationsDbContext))]
-    partial class GrpcDemoMigrationsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240104201038_Updated_To_v8")]
+    partial class Updated_To_v8
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

db586e51c5e0815842f93fe91ccba2cdbe19ed04
maliming <malimings@gmail.com>
2024-01-05 11:37:39 +0800

    Update Index.razor.cs

--- GrpcDemo2/src/ProductManagement.Blazor/Pages/Index.razor.cs
+++ GrpcDemo2/src/ProductManagement.Blazor/Pages/Index.razor.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Grpc.Core;
 using Grpc.Net.Client;
 using Grpc.Net.Client.Configuration;
 using Grpc.Net.Client.Web;
@@ -17,9 +18,18 @@ public partial class Index
 
     protected override async Task OnInitializedAsync()
     {
+        var credentials = CallCredentials.FromInterceptor(async (context, metadata) =>
+        {
+            // var token = "get access token from somewhere
+            // metadata.Add("Authorization", $"Bearer {token}");
+
+            metadata.Add("__tenant", ProductManagementConstants.AcmeTenant);
+        });
+
         var channel = GrpcChannel.ForAddress("https://localhost:10042", new GrpcChannelOptions
         {
-            HttpHandler = new GrpcWebHandler(new HttpClientHandler())
+            HttpHandler = new GrpcWebHandler(new HttpClientHandler()),
+            Credentials = ChannelCredentials.Create(new SslCredentials(), credentials)
         });
 
         var productAppService = channel.CreateGrpcService<IProductAppService>();

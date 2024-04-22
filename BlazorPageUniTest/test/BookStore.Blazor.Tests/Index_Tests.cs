0e6ab73ecb35609f4ff459cf1fd54c75b0a2b6f5
maliming <malimings@gmail.com>
2024-04-22 16:57:28 +0800

    Update `BlazorPageUniTest` sample.

--- BlazorPageUniTest/test/BookStore.Blazor.Tests/Index_Tests.cs
+++ BlazorPageUniTest/test/BookStore.Blazor.Tests/Index_Tests.cs
@@ -9,14 +9,14 @@ public class Index_Tests : BookStoreBlazorTestBase
     [Fact]
     public void Index_Test()
     {
-        using (var ctx = CreateTestContext())
-        {
-            // Act
-            var cut = ctx.RenderComponent<BookStore.Blazor.Pages.Index>();
+        // Arrange
+        var ctx = CreateTestContext();
 
-            // Assert
-            cut.Find(".lead").InnerHtml.Contains("Welcome to the application. This is a startup project based on the ABP framework. For more information, visit abp.io.").ShouldBeTrue();
-            cut.Find("#username").InnerHtml.Contains("Welcome admin").ShouldBeTrue();
-        }
+        // Act
+        var cut = ctx.RenderComponent<BookStore.Blazor.Pages.Index>();
+
+        // Assert
+        cut.Find(".lead").InnerHtml.Contains("Welcome to the application. This is a startup project based on the ABP framework. For more information, visit abp.io.").ShouldBeTrue();
+        cut.Find("#username").InnerHtml.Contains("Welcome admin").ShouldBeTrue();
     }
 }

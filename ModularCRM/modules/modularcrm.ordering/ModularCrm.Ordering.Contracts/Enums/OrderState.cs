2e26a096e68eb6b6c68392b3100a55dfbd72f967
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2024-09-05 15:50:19 +0300

    Created order entity

---
+++ ModularCRM/modules/modularcrm.ordering/ModularCrm.Ordering.Contracts/Enums/OrderState.cs
@@ -0,0 +1,8 @@
+﻿namespace ModularCrm.Ordering.Contracts.Enums;
+
+public enum OrderState : byte
+{
+    Placed = 0,
+    Delivered = 1,
+    Canceled = 2
+}
---
+++ ModularCRM/modules/modularcrm.ordering/ModularCrm.Ordering/Entities/Order.cs
@@ -0,0 +1,13 @@
+﻿using Acme.Ecommerce.OrderingService.Enums;
+using System;
+using Volo.Abp.Domain.Entities;
+
+namespace ModularCrm.Ordering.Entities
+{
+    public class Order : AggregateRoot<Guid>
+    {
+        public Guid ProductId { get; set; }
+        public string CustomerName { get; set; }
+        public OrderState State { get; set; }
+    }
+}

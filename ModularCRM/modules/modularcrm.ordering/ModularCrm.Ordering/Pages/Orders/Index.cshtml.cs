50e5268d9115617e5216f58127270c7c22286ba9
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2024-09-06 17:53:28 +0300

    Created Orders page

---
+++ ModularCRM/modules/modularcrm.ordering/ModularCrm.Ordering/Pages/Orders/Index.cshtml.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ModularCrm.Ordering.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Repositories;
+
+namespace ModularCrm.Ordering.Pages.Orders
+{
+    public class IndexModel : PageModel
+    {
+        public List<Order> Orders { get; set; }
+
+        private readonly IRepository<Order, Guid> _orderRepository;
+
+        public IndexModel(IRepository<Order, Guid> orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+
+        public async Task OnGetAsync()
+        {
+            Orders = await _orderRepository.GetListAsync();
+        }
+    }
+}

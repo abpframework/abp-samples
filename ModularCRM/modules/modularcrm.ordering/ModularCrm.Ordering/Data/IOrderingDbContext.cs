663adce4c0cfe00ad97d4a3cca8ea2ad0909df19
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2024-09-06 16:36:48 +0300

    Created IOrderingDbContext

---
+++ ModularCRM/modules/modularcrm.ordering/ModularCrm.Ordering/Data/IOrderingDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ModularCrm.Ordering.Entities;
+using Volo.Abp.EntityFrameworkCore;
+
+namespace ModularCrm.Ordering.Data
+{
+    public interface IOrderingDbContext : IEfCoreDbContext
+    {
+        DbSet<Order> Orders { get; set; }
+    }
+}
---
+++ ModularCRM/modules/modularcrm.ordering/ModularCrm.Ordering/Data/OrderingDbContextModelCreatingExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ModularCrm.Ordering.Entities;
+using Volo.Abp;
+using Volo.Abp.EntityFrameworkCore.Modeling;
+
+namespace ModularCrm.Ordering.Data
+{
+    public static class OrderingDbContextModelCreatingExtensions
+    {
+        public static void ConfigureOrdering(this ModelBuilder builder)
+        {
+            Check.NotNull(builder, nameof(builder));
+
+            builder.Entity<Order>(b =>
+            {
+                //Configure table name
+                b.ToTable("Orders");
+
+                //Always call this method to setup base entity properties
+                b.ConfigureByConvention();
+
+                //Properties of the entity
+                b.Property(q => q.CustomerName).IsRequired().HasMaxLength(120);
+            });
+        }
+    }
+}

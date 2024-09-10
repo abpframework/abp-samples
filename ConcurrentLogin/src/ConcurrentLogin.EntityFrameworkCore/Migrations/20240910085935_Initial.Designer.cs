2bb22b0986571edf0b3125f668481dc8000fb03c
liangshiwei <shiwei.liang@volosoft.com>
2024-09-10 17:16:17 +0800

    Add initial migration file

--- ConcurrentLogin/src/ConcurrentLogin.EntityFrameworkCore/Migrations/20240910085935_Initial.Designer.cs
+++ ConcurrentLogin/src/ConcurrentLogin.EntityFrameworkCore/Migrations/20240910085935_Initial.Designer.cs
@@ -4,6 +4,7 @@ using ConcurrentLogin.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Volo.Abp.EntityFrameworkCore;
 
@@ -12,9 +13,11 @@ using Volo.Abp.EntityFrameworkCore;
 namespace ConcurrentLogin.Migrations
 {
     [DbContext(typeof(ConcurrentLoginDbContext))]
-    partial class ConcurrentLoginDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240910085935_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

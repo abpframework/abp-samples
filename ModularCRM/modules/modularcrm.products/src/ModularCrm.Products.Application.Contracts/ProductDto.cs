53b1430ebe9b93acffa933a0fd8550b734f384b5
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2024-09-17 14:10:34 +0300

    Added missing property

--- ModularCRM/modules/modularcrm.products/src/ModularCrm.Products.Application.Contracts/ProductDto.cs
+++ ModularCRM/modules/modularcrm.products/src/ModularCrm.Products.Application.Contracts/ProductDto.cs
@@ -1,9 +1,11 @@
-﻿namespace ModularCrm.Products
+﻿using System;
+
+namespace ModularCrm.Products
 {
     public class ProductDto
     {
+        public Guid Id { get; set; }
         public string Name { get; set; }
-
         public int StockCount { get; set; }
     }
 }

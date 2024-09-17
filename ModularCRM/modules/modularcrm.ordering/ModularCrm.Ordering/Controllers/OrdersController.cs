960358d36dfb80406a5fc751ce6235d816917873
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2024-09-17 22:13:27 +0300

    Added OrdersController

---
+++ ModularCRM/modules/modularcrm.ordering/ModularCrm.Ordering/Controllers/OrdersController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModularCrm.Ordering.Contracts.Enums;
+using ModularCrm.Ordering.Contracts.Events;
+using ModularCrm.Ordering.Entities;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using Volo.Abp.AspNetCore.Mvc;
+using Volo.Abp.Domain.Repositories;
+using Volo.Abp.EventBus.Distributed;
+
+namespace ModularCrm.Ordering.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : AbpControllerBase
+    {
+        private readonly IRepository<Order, Guid> _orderRepository;
+        private readonly IDistributedEventBus _distributedEventBus;
+
+        public OrdersController(
+            IRepository<Order, Guid> orderRepository,
+            IDistributedEventBus distributedEventBus)
+        {
+            _orderRepository = orderRepository;
+            _distributedEventBus = distributedEventBus;
+        }
+
+        [HttpPost]
+        public async Task CreateAsync(OrderCreationModel input)
+        {
+            var order = new Order
+            {
+                CustomerName = input.CustomerName,
+                ProductId = input.ProductId,
+                State = OrderState.Placed
+            };
+
+            await _orderRepository.InsertAsync(order);
+
+            await _distributedEventBus.PublishAsync(
+                new OrderPlacedEto
+                {
+                    ProductId = order.ProductId,
+                    CustomerName = order.CustomerName
+                });
+        }
+
+        public class OrderCreationModel
+        {
+            public Guid ProductId { get; set; }
+
+            [Required]
+            [StringLength(120)]
+            public string CustomerName { get; set; }
+        }
+    }
+}

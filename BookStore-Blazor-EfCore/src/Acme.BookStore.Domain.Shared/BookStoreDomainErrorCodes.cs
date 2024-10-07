0bda164f1dd95a52994529e1e014f91a0fd282ed
Fahri Gedik <fahrican9978@gmail.com>
2024-10-07 12:54:16 +0300

    Completed Part 6: Authors: Domain Layer

--- BookStore-Blazor-EfCore/src/Acme.BookStore.Domain.Shared/BookStoreDomainErrorCodes.cs
+++ BookStore-Blazor-EfCore/src/Acme.BookStore.Domain.Shared/BookStoreDomainErrorCodes.cs
@@ -3,4 +3,5 @@
 public static class BookStoreDomainErrorCodes
 {
     /* You can add your business exception error codes here, as constants */
+    public const string AuthorAlreadyExists = "BookStore:00001";
 }
---
+++ BookStore-Blazor-EfCore/src/Acme.BookStore.Domain/Authors/AuthorManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Volo.Abp;
+using Volo.Abp.Domain.Services;
+
+namespace Acme.BookStore.Authors;
+
+public class AuthorManager : DomainService
+{
+    private readonly IAuthorRepository _authorRepository;
+
+    public AuthorManager(IAuthorRepository authorRepository)
+    {
+        _authorRepository = authorRepository;
+    }
+
+    public async Task<Author> CreateAsync(
+        string name,
+        DateTime birthDate,
+        string? shortBio = null)
+    {
+        Check.NotNullOrWhiteSpace(name, nameof(name));
+
+        var existingAuthor = await _authorRepository.FindByNameAsync(name);
+        if (existingAuthor != null)
+        {
+            throw new AuthorAlreadyExistsException(name);
+        }
+
+        return new Author(
+            GuidGenerator.Create(),
+            name,
+            birthDate,
+            shortBio
+        );
+    }
+
+    public async Task ChangeNameAsync(
+        Author author,
+        string newName)
+    {
+        Check.NotNull(author, nameof(author));
+        Check.NotNullOrWhiteSpace(newName, nameof(newName));
+
+        var existingAuthor = await _authorRepository.FindByNameAsync(newName);
+        if (existingAuthor != null && existingAuthor.Id != author.Id)
+        {
+            throw new AuthorAlreadyExistsException(newName);
+        }
+
+        author.ChangeName(newName);
+    }
+}

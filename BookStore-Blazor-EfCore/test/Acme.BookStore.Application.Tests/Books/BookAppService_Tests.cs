d6030aded830a0cca6b11fd55e19288a1668a24f
Fahri Gedik <fahrican9978@gmail.com>
2024-10-17 01:47:21 +0300

    Completed - Part 4: Integration Tests

---
+++ BookStore-Blazor-EfCore/test/Acme.BookStore.Application.Tests/Books/BookAppService_Tests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Shouldly;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Modularity;
+using Volo.Abp.Validation;
+using Xunit;
+
+namespace Acme.BookStore.Books;
+
+public abstract class BookAppService_Tests<TStartupModule> : BookStoreApplicationTestBase<TStartupModule>
+    where TStartupModule : IAbpModule
+{
+    private readonly IBookAppService _bookAppService;
+
+    protected BookAppService_Tests()
+    {
+        _bookAppService = GetRequiredService<IBookAppService>();
+    }
+
+    [Fact]
+    public async Task Should_Get_List_Of_Books()
+    {
+        //Act
+        var result = await _bookAppService.GetListAsync(
+            new PagedAndSortedResultRequestDto()
+        );
+
+        //Assert
+        result.TotalCount.ShouldBeGreaterThan(0);
+        result.Items.ShouldContain(b => b.Name == "1984");
+    }
+
+    [Fact]
+    public async Task Should_Create_A_Valid_Book()
+    {
+        //Act
+        var result = await _bookAppService.CreateAsync(
+            new CreateUpdateBookDto
+            {
+                Name = "New test book 42",
+                Price = 10,
+                PublishDate = DateTime.Now,
+                Type = BookType.ScienceFiction
+            }
+        );
+
+        //Assert
+        result.Id.ShouldNotBe(Guid.Empty);
+        result.Name.ShouldBe("New test book 42");
+    }
+
+    [Fact]
+    public async Task Should_Not_Create_A_Book_Without_Name()
+    {
+        var exception = await Assert.ThrowsAsync<AbpValidationException>(async () =>
+        {
+            await _bookAppService.CreateAsync(
+                new CreateUpdateBookDto
+                {
+                    Name = "",
+                    Price = 10,
+                    PublishDate = DateTime.Now,
+                    Type = BookType.ScienceFiction
+                }
+            );
+        });
+
+        exception.ValidationErrors
+            .ShouldContain(err => err.MemberNames.Any(mem => mem == "Name"));
+    }
+}

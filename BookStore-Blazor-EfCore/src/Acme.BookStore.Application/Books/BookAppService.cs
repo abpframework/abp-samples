fc8db42c9eeff0ea24e4d92005685ecdb489cefd
Fahri Gedik <fahrican9978@gmail.com>
2024-10-17 02:52:08 +0300

    Completed - Part 10: Book to Author Relation

--- BookStore-Blazor-EfCore/src/Acme.BookStore.Application/Books/BookAppService.cs
+++ BookStore-Blazor-EfCore/src/Acme.BookStore.Application/Books/BookAppService.cs
@@ -1,11 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Dynamic.Core;
+using System.Threading.Tasks;
+using Acme.BookStore.Authors;
 using Acme.BookStore.Permissions;
+using Microsoft.AspNetCore.Authorization;
 using Volo.Abp.Application.Dtos;
 using Volo.Abp.Application.Services;
+using Volo.Abp.Domain.Entities;
 using Volo.Abp.Domain.Repositories;
 
 namespace Acme.BookStore.Books;
 
+[Authorize(BookStorePermissions.Books.Default)]
 public class BookAppService :
     CrudAppService<
         Book, //The Book entity
@@ -15,13 +23,105 @@ public class BookAppService :
         CreateUpdateBookDto>, //Used to create/update a book
     IBookAppService //implement the IBookAppService
 {
-    public BookAppService(IRepository<Book, Guid> repository)
+    private readonly IAuthorRepository _authorRepository;
+
+    public BookAppService(
+        IRepository<Book, Guid> repository,
+        IAuthorRepository authorRepository)
         : base(repository)
     {
+        _authorRepository = authorRepository;
         GetPolicyName = BookStorePermissions.Books.Default;
         GetListPolicyName = BookStorePermissions.Books.Default;
         CreatePolicyName = BookStorePermissions.Books.Create;
         UpdatePolicyName = BookStorePermissions.Books.Edit;
         DeletePolicyName = BookStorePermissions.Books.Delete;
+    }
+
+    public override async Task<BookDto> GetAsync(Guid id)
+    {
+        //Get the IQueryable<Book> from the repository
+        var queryable = await Repository.GetQueryableAsync();
+
+        //Prepare a query to join books and authors
+        var query = from book in queryable
+                    join author in await _authorRepository.GetQueryableAsync() on book.AuthorId equals author.Id
+                    where book.Id == id
+                    select new { book, author };
+
+        //Execute the query and get the book with author
+        var queryResult = await AsyncExecuter.FirstOrDefaultAsync(query);
+        if (queryResult == null)
+        {
+            throw new EntityNotFoundException(typeof(Book), id);
+        }
+
+        var bookDto = ObjectMapper.Map<Book, BookDto>(queryResult.book);
+        bookDto.AuthorName = queryResult.author.Name;
+        return bookDto;
+    }
+
+    public override async Task<PagedResultDto<BookDto>> GetListAsync(PagedAndSortedResultRequestDto input)
+    {
+        //Get the IQueryable<Book> from the repository
+        var queryable = await Repository.GetQueryableAsync();
+
+        //Prepare a query to join books and authors
+        var query = from book in queryable
+                    join author in await _authorRepository.GetQueryableAsync() on book.AuthorId equals author.Id
+                    select new { book, author };
+
+        //Paging
+        query = query
+            .OrderBy(NormalizeSorting(input.Sorting))
+            .Skip(input.SkipCount)
+            .Take(input.MaxResultCount);
+
+        //Execute the query and get a list
+        var queryResult = await AsyncExecuter.ToListAsync(query);
+
+        //Convert the query result to a list of BookDto objects
+        var bookDtos = queryResult.Select(x =>
+        {
+            var bookDto = ObjectMapper.Map<Book, BookDto>(x.book);
+            bookDto.AuthorName = x.author.Name;
+            return bookDto;
+        }).ToList();
+
+        //Get the total count with another query
+        var totalCount = await Repository.GetCountAsync();
+
+        return new PagedResultDto<BookDto>(
+            totalCount,
+            bookDtos
+        );
+    }
+
+    public async Task<ListResultDto<AuthorLookupDto>> GetAuthorLookupAsync()
+    {
+        var authors = await _authorRepository.GetListAsync();
+
+        return new ListResultDto<AuthorLookupDto>(
+            ObjectMapper.Map<List<Author>, List<AuthorLookupDto>>(authors)
+        );
+    }
+
+    private static string NormalizeSorting(string sorting)
+    {
+        if (sorting.IsNullOrEmpty())
+        {
+            return $"book.{nameof(Book.Name)}";
+        }
+
+        if (sorting.Contains("authorName", StringComparison.OrdinalIgnoreCase))
+        {
+            return sorting.Replace(
+                "authorName",
+                "author.Name",
+                StringComparison.OrdinalIgnoreCase
+            );
+        }
+
+        return $"book.{sorting}";
     }
 }
--- BookStore-Blazor-EfCore/src/Acme.BookStore.Domain/Books/Book.cs
+++ BookStore-Blazor-EfCore/src/Acme.BookStore.Domain/Books/Book.cs
@@ -12,4 +12,7 @@ public class Book : AuditedAggregateRoot<Guid>
     public DateTime PublishDate { get; set; }
 
     public float Price { get; set; }
+
+    public Guid AuthorId { get; set; }
+
 }

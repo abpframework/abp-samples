1700c6398eb3127c16a39c2a7de9a04b0a9fb620
Fahri Gedik <fahrican9978@gmail.com>
2024-10-17 02:07:00 +0300

    Completed - Part 6: Authors: Domain Layer

---
+++ BookStore-Blazor-EfCore/src/Acme.BookStore.Domain/Authors/Author.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using JetBrains.Annotations;
+using Volo.Abp;
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace Acme.BookStore.Authors;
+
+public class Author : FullAuditedAggregateRoot<Guid>
+{
+    public string Name { get; private set; }
+    public DateTime BirthDate { get; set; }
+    public string ShortBio { get; set; }
+
+    private Author()
+    {
+        /* This constructor is for deserialization / ORM purpose */
+    }
+
+    internal Author(
+        Guid id,
+        string name,
+        DateTime birthDate,
+        string? shortBio = null)
+        : base(id)
+    {
+        SetName(name);
+        BirthDate = birthDate;
+        ShortBio = shortBio;
+    }
+
+    internal Author ChangeName(string name)
+    {
+        SetName(name);
+        return this;
+    }
+
+    private void SetName(string name)
+    {
+        Name = Check.NotNullOrWhiteSpace(
+            name,
+            nameof(name),
+            maxLength: AuthorConsts.MaxNameLength
+        );
+    }
+}
---
+++ BookStore-Blazor-EfCore/src/Acme.BookStore.Domain/Authors/AuthorAlreadyExistsException.cs
@@ -0,0 +1,12 @@
+﻿using Volo.Abp;
+
+namespace Acme.BookStore.Authors;
+
+public class AuthorAlreadyExistsException : BusinessException
+{
+    public AuthorAlreadyExistsException(string name)
+        : base(BookStoreDomainErrorCodes.AuthorAlreadyExists)
+    {
+        WithData("name", name);
+    }
+}

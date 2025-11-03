7b920429aa1495f8fcf21cba732240d552e580f4
erdemcaygor <erdem.caygor@gmail.com>
2025-11-03 16:01:11 +0300

    migrations applied

--- TodoApp/Angular-EfCore/angular/src/app/app.config.ts
+++ TodoApp/Angular-EfCore/angular/src/app/app.config.ts
@@ -15,6 +15,7 @@ import {provideThemeLeptonX} from "@abp/ng.theme.lepton-x";
 import {provideTenantManagementConfig} from "@abp/ng.tenant-management/config";
 import {provideIdentityConfig} from "@abp/ng.identity/config";
 import {provideAccountConfig} from "@abp/ng.account/config";
+import {provideSideMenuLayout} from "@abp/ng.theme.lepton-x/layouts";
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -31,6 +32,7 @@ export const appConfig: ApplicationConfig = {
     provideSettingManagementConfig(),
     provideFeatureManagementConfig(),
     provideAbpThemeShared(),
+    provideSideMenuLayout(),
     provideThemeLeptonX(),
     provideTenantManagementConfig(),
     provideIdentityConfig(),

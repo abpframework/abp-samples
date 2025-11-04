0d82f26843eeb08e756106069412338d34ed623f
erdemcaygor <erdem.caygor@gmail.com>
2025-11-04 10:02:22 +0300

    refactoring

--- TodoApp-SingleLayer/Angular-EfCore/angular/src/app/app.component.ts
+++ TodoApp-SingleLayer/Angular-EfCore/angular/src/app/app.component.ts
@@ -5,8 +5,8 @@ import {DynamicLayoutComponent} from "@abp/ng.core";
 @Component({
   selector: 'app-root',
   template: `
-    <abp-loader-bar></abp-loader-bar>
-    <abp-dynamic-layout></abp-dynamic-layout>
+    <abp-loader-bar/>
+    <abp-dynamic-layout/>
   `,
   imports: [LoaderBarComponent, DynamicLayoutComponent]
 })

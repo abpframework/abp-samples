ef18ea9f466bc41405def464275e705359434f5e
sumeyye <sum.the.developer@gmail.com>
2025-11-12 08:58:32 +0300

    update: proxies to use injection function instead

--- TodoApp-SingleLayer/Angular-Mongo/angular/src/app/app.config.ts
+++ TodoApp-SingleLayer/Angular-Mongo/angular/src/app/app.config.ts
@@ -10,12 +10,12 @@ import { provideRouter } from '@angular/router';
 import { environment } from '../environments/environment';
 import { APP_ROUTES } from './app.routes';
 import { APP_ROUTE_PROVIDER } from './route.provider';
-import {registerLocale} from '@abp/ng.core/locale';
-import {provideThemeLeptonX} from "@abp/ng.theme.lepton-x";
-import {provideTenantManagementConfig} from "@abp/ng.tenant-management/config";
-import {provideIdentityConfig} from "@abp/ng.identity/config";
-import {provideAccountConfig} from "@abp/ng.account/config";
-import {provideSideMenuLayout} from "@abp/ng.theme.lepton-x/layouts";
+import { registerLocaleForEsBuild } from '@abp/ng.core/locale';
+import { provideThemeLeptonX } from '@abp/ng.theme.lepton-x';
+import { provideTenantManagementConfig } from '@abp/ng.tenant-management/config';
+import { provideIdentityConfig } from '@abp/ng.identity/config';
+import { provideAccountConfig } from '@abp/ng.account/config';
+import { provideSideMenuLayout } from '@abp/ng.theme.lepton-x/layouts';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -25,8 +25,8 @@ export const appConfig: ApplicationConfig = {
     provideAbpCore(
       withOptions({
         environment,
-        registerLocaleFn: registerLocale(),
-      }),
+        registerLocaleFn: registerLocaleForEsBuild(),
+      })
     ),
     provideAbpOAuth(),
     provideSettingManagementConfig(),
@@ -37,6 +37,6 @@ export const appConfig: ApplicationConfig = {
     provideTenantManagementConfig(),
     provideIdentityConfig(),
     provideAccountConfig(),
-    provideLogo(withEnvironmentOptions(environment))
-  ]
+    provideLogo(withEnvironmentOptions(environment)),
+  ],
 };

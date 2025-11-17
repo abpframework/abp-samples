5afdaa2114a689f8f31dbb6b03090e8bb1743428
sumeyye <sum.the.developer@gmail.com>
2025-11-17 11:28:13 +0300

    update: replace guards with the new ones

--- BookStore-Angular-MongoDb/angular/src/app/route.provider.ts
+++ BookStore-Angular-MongoDb/angular/src/app/route.provider.ts
@@ -3,36 +3,40 @@ import { RoutesService, eLayoutType } from '@abp/ng.core';
 
 export const APP_ROUTE_PROVIDER = [
   provideAppInitializer(() => {
-    const routesService = inject(RoutesService);
-    routesService.add([
-      {
-        path: '/',
-        name: '::Menu:Home',
-        iconClass: 'fas fa-home',
-        order: 1,
-        layout: eLayoutType.application,
-      },
-      {
-        path: '/book-store',
-        name: '::Menu:BookStore',
-        iconClass: 'fas fa-book',
-        order: 2,
-        layout: eLayoutType.application,
-      },
-      {
-        path: '/books',
-        name: '::Menu:Books',
-        parentName: '::Menu:BookStore',
-        layout: eLayoutType.application,
-        requiredPolicy: 'BookStore.Books',
-      },
-      {
-        path: '/authors',
-        name: '::Menu:Authors',
-        parentName: '::Menu:BookStore',
-        layout: eLayoutType.application,
-        requiredPolicy: 'BookStore.Authors',
-      },
-    ]);
-  })
+    configureRoutes();
+  }),
 ];
+
+function configureRoutes() {
+  const routesService = inject(RoutesService);
+  routesService.add([
+    {
+      path: '/',
+      name: '::Menu:Home',
+      iconClass: 'fas fa-home',
+      order: 1,
+      layout: eLayoutType.application,
+    },
+    {
+      path: '/book-store',
+      name: '::Menu:BookStore',
+      iconClass: 'fas fa-book',
+      order: 2,
+      layout: eLayoutType.application,
+    },
+    {
+      path: '/books',
+      name: '::Menu:Books',
+      parentName: '::Menu:BookStore',
+      layout: eLayoutType.application,
+      requiredPolicy: 'BookStore.Books',
+    },
+    {
+      path: '/authors',
+      name: '::Menu:Authors',
+      parentName: '::Menu:BookStore',
+      layout: eLayoutType.application,
+      requiredPolicy: 'BookStore.Authors',
+    },
+  ]);
+}
